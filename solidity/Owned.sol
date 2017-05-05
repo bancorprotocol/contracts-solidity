0c72c0d75abfabcf500b97ad8e16a18934187e52
yudilevi <yudilevi@gmail.com>
2017-05-05 22:40:00 +0300

    ownership transfer now requires the new owner's to accept it

--- solidity/Owned.sol
+++ solidity/Owned.sol
@@ -2,6 +2,7 @@ pragma solidity ^0.4.10;
 
 contract Owned {
     address public owner;
+    address public newOwner;
 
     event OwnerUpdate(address _prevOwner, address _newOwner);
 
@@ -17,12 +18,22 @@ contract Owned {
 
     /*
         allows transferring the contract ownership
+        the new owner still need to accept the transfer
         can only be called by the contract owner
     */
     function setOwner(address _newOwner) public ownerOnly {
         require(_newOwner != owner);
+        newOwner = _newOwner;
+    }
+
+    /*
+        used by a new owner to accept an ownership transfer
+    */
+    function acceptOwnership() public {
+        require(msg.sender == newOwner);
         address prevOwner = owner;
-        owner = _newOwner;
+        owner = newOwner;
+        newOwner = 0x0;
         OwnerUpdate(prevOwner, owner);
     }
 }
