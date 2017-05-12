6bc6677949b90433e7ce1fa01e8e30f6f9d9f626
yudilevi <yudilevi@gmail.com>
2017-05-12 22:10:35 +0300

    renamed tests folder to test

--- truffle.js
+++ truffle.js
@@ -3,7 +3,8 @@ module.exports = {
     development: {
       host: "localhost",
       port: 8545,
-      network_id: "*" // Match any network id
+      network_id: "*", // Match any network id
+      from: "0x00a329c0648769A73afAc7F9381E08FB43dBEA72"
     }
   }
 };
