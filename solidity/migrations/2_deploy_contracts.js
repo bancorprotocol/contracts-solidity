b1137aaa2536973339e8e4cd07d1129eefc0879c
yudilevi <yudilevi@gmail.com>
2017-05-16 00:35:17 +0300

    added Owned tests

--- solidity/migrations/2_deploy_contracts.js
+++ solidity/migrations/2_deploy_contracts.js
@@ -2,12 +2,14 @@
 /* eslint-disable prefer-reflect */
 
 const SafeMath = artifacts.require('SafeMath.sol');
+const Owned = artifacts.require('Owned.sol');
 const BancorFormula = artifacts.require('BancorFormula.sol');
 const ERC20Token = artifacts.require('ERC20Token.sol');
 const EtherToken = artifacts.require('EtherToken.sol');
 
 module.exports = (deployer) => {
     deployer.deploy(SafeMath);
+    deployer.deploy(Owned);
     deployer.deploy(BancorFormula);
     deployer.deploy(ERC20Token, 'DummyToken', 'DUM');
     deployer.deploy(EtherToken);
---
+++ solidity/test/Owned.js
@@ -0,0 +1,55 @@
+/* global artifacts, contract, it, assert, web3 */
+/* eslint-disable prefer-reflect */
+
+const Owned = artifacts.require('Owned.sol');
+const utils = require('./Utils');
+
+contract('Owned', (accounts) => {
+    it('verifies the owner after construction', async () => {
+        let contract = await Owned.new();
+        let owner = await contract.owner.call();
+        assert.equal(owner, accounts[0]);
+    });
+
+    it('verifies the new owner after ownership transfer', async () => {
+        let contract = await Owned.new();
+        await contract.setOwner(accounts[1]);
+        await contract.acceptOwnership({ from: accounts[1] });
+        let owner = await contract.owner.call();
+        assert.equal(owner, accounts[1]);
+    });
+
+    it('verifies that ownership transfer fires an OwnerUpdate event', async () => {
+        let contract = await Owned.new();
+        await contract.setOwner(accounts[1]);
+        let res = await contract.acceptOwnership({ from: accounts[1] });
+        assert(res.logs.length > 0 && res.logs[0].event == 'OwnerUpdate');
+    });
+
+    it('verifies that newOwner is cleared after ownership transfer', async () => {
+        let contract = await Owned.new();
+        await contract.setOwner(accounts[1]);
+        await contract.acceptOwnership({ from: accounts[1] });
+        let newOwner = await contract.newOwner.call();
+        assert.equal(web3.toBigNumber(newOwner).toNumber(), 0);
+    });
+
+    it('verifies that no ownership transfer takes places before the new owner accepted it', async () => {
+        let contract = await Owned.new();
+        await contract.setOwner(accounts[1]);
+        let owner = await contract.owner.call();
+        assert.equal(owner, accounts[0]);
+    });
+
+    it('verifies that only the owner can initiate ownership transfer', async () => {
+        let contract = await Owned.new();
+
+        try {
+            await contract.setOwner(accounts[1], { from: accounts[2] });
+            assert(false, "didn't throw");
+        }
+        catch (error) {
+            return utils.ensureException(error);
+        }
+    });
+});
