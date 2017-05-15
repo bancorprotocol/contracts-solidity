1cfb8d0957462a7f7fe289471b9e7863d32e2aab
yudilevi <yudilevi@gmail.com>
2017-05-15 23:57:39 +0300

    added SafeMath tests

--- solidity/migrations/2_deploy_contracts.js
+++ solidity/migrations/2_deploy_contracts.js
@@ -1,11 +1,13 @@
 /* global artifacts */
 /* eslint-disable prefer-reflect */
 
+const SafeMath = artifacts.require('SafeMath.sol');
 const BancorFormula = artifacts.require('BancorFormula.sol');
 const ERC20Token = artifacts.require('ERC20Token.sol');
 const EtherToken = artifacts.require('EtherToken.sol');
 
 module.exports = (deployer) => {
+    deployer.deploy(SafeMath);
     deployer.deploy(BancorFormula);
     deployer.deploy(ERC20Token, 'DummyToken', 'DUM');
     deployer.deploy(EtherToken);
