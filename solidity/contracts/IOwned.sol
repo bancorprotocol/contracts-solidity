cd95f8204ba30afe94bc0d5c27a0d34b2001941e
yudilevi <yudilevi@gmail.com>
2017-05-21 12:30:48 +0300

    added the Owned interface

---
+++ solidity/contracts/IOwned.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.4.10;
+
+/*
+    Owned contract interface
+*/
+contract IOwned {
+    // this function isn't abstract since the compiler emits automatically generated getter functions as external
+    function owner() public constant returns (address owner) {}
+}
