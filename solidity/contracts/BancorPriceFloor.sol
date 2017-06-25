afd5da8adf7fd4aa57529ba3ad2d0f3526075c93
yudilevi <yudilevi@gmail.com>
2017-06-25 17:56:36 +0300

    BancorPriceFloor - added SafeMath dependency, added payable fallback function

--- solidity/contracts/BancorPriceFloor.sol
+++ solidity/contracts/BancorPriceFloor.sol
@@ -1,5 +1,6 @@
 pragma solidity ^0.4.11;
 import './TokenHolder.sol';
+import './SafeMath.sol';
 import './ISmartToken.sol';
 
 /*
@@ -7,9 +8,9 @@ import './ISmartToken.sol';
 
     The bancor price floor contract is a simple contract that allows selling smart tokens for a constant ETH price
 */
-contract BancorPriceFloor is TokenHolder {
-    uint256 public constant TOKEN_PRICE_N = 1;      // price in wei (numerator)
-    uint256 public constant TOKEN_PRICE_D = 100;    // price in wei (denominator)
+contract BancorPriceFloor is TokenHolder, SafeMath {
+    uint256 public constant TOKEN_PRICE_N = 1;      // crowdsale price in wei (numerator)
+    uint256 public constant TOKEN_PRICE_D = 100;    // crowdsale price in wei (denominator)
 
     string public version = '0.1';
     ISmartToken public token; // smart token the contract allows selling
@@ -34,9 +35,9 @@ contract BancorPriceFloor is TokenHolder {
     function sell() public returns (uint256 amount) {
         uint256 allowance = token.allowance(msg.sender, this); // get the full allowance amount
         assert(token.transferFrom(msg.sender, this, allowance)); // transfer all tokens from the sender to the contract
-        uint256 ethValue = allowance * TOKEN_PRICE_N / TOKEN_PRICE_D; // calculate ETH value of the tokens
-        assert(msg.sender.send(ethValue)); // send the ETH amount to the seller
-        return ethValue;
+        uint256 etherValue = safeMul(allowance, TOKEN_PRICE_N) / TOKEN_PRICE_D; // calculate ETH value of the tokens
+        assert(msg.sender.send(etherValue)); // send the ETH amount to the seller
+        return etherValue;
     }
 
     /**
@@ -46,5 +47,11 @@ contract BancorPriceFloor is TokenHolder {
     */
     function withdraw(uint256 _amount) public ownerOnly {
         assert(msg.sender.send(_amount)); // send the amount
+    }
+
+    /**
+        @dev deposits ETH in the contract
+    */
+    function() public payable {
     }
 }
