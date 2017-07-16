010f42f6bfa97badc2b543f27424210855b207f6
barakman <barakman@yahoo.com>
2017-07-16 17:34:29 +0300

    Align python emulation with solidity contract

--- solidity/python/Formula/Power/__init__.py
+++ solidity/python/Formula/Power/__init__.py
@@ -1,8 +1,37 @@
+ONE = 1;
+TWO = 2;
 MAX_FIXED_EXP_32 = 0x386bfdba29;
 
 
 '''
-    @dev Calculate (_baseN / _baseD) ^ (_expN / _expD)
+    calculateBestPrecision 
+    Predicts the highest precision which can be used in order to compute "base^exp" without exceeding 256 bits in any of the intermediate computations.
+    Instead of calculating "base ^ exp", we calculate "e ^ (ln(base) * exp)".
+    The value of ln(base) is represented with an integer slightly smaller than ln(base) * 2 ^ precision.
+    The larger the precision is, the more accurately this value represents the real value.
+    However, function fixedExpUnsafe(x), which calculates e ^ x, is limited to a maximum value of x.
+    The limit depends on the precision (e.g, for precision = 32, the maximum value of x is MAX_FIXED_EXP_32).
+    Hence before calling the 'power' function, we need to estimate an upper-bound for ln(base) * exponent.
+    Of course, we should later assert that the value passed to fixedExpUnsafe is not larger than MAX_FIXED_EXP(precision).
+    Due to this assertion (made in function fixedExp), functions calculateBestPrecision and fixedExp are tightly coupled.
+    Note that the outcome of this function only affects the accuracy of the computation of "base ^ exp".
+    Therefore, there is no need to assert that no intermediate result exceeds 256 bits (nor in this function, neither in any of the functions down the calling tree).
+'''
+def calculateBestPrecision(_baseN, _baseD, _expN, _expD):
+    maxExp = MAX_FIXED_EXP_32;
+    maxVal = _expN * lnUpperBound(_baseN,_baseD);
+    for precision in range(32, 64, 2):
+        if (maxExp < (maxVal << precision) / _expD):
+            break;
+        maxExp = (maxExp * 0xeb5ec5975959c565) >> (64-2);
+    else:
+        return 64-2;
+    if (precision == 32):
+        return 32;
+    return precision-2;
+
+'''
+    @dev calculates (_baseN / _baseD) ^ (_expN / _expD)
     Returns result upshifted by precision
 
     This method is overflow-safe
@@ -33,11 +62,32 @@ def ln(_numerator, _denominator, _precision):
     assert(_denominator != 0 and _numerator != 0);
 
     # Upper bits are scaled off by precision
-    MAX_VAL = (1) << (256 - _precision);
+    MAX_VAL = ONE << (256 - _precision);
     assert(_numerator < MAX_VAL);
     assert(_denominator < MAX_VAL);
 
     return fixedLoge( (_numerator << _precision) / _denominator, _precision);
+
+'''
+    lnUpperBound 
+    Takes a rational number (baseN / baseD) as input.
+    Returns an estimated upper-bound integer of the natural logarithm of the input.
+    We can generally use floorLog2, although there is possibly a tighter bound.
+    We cannot use floorLog2 when the input is smaller than 8.
+    Complexity is O(log(input bit - length)).
+'''
+def lnUpperBound(_baseN, _baseD):
+    assert(_baseN > _baseD);
+
+    scaledBaseN = _baseN * 100000;
+    if (scaledBaseN <= _baseD *  271828): # _baseN / _baseD < e^1 (floorLog2 will return 0 if _baseN / _baseD < 2)
+        return 1;
+    if (scaledBaseN <= _baseD *  738905): # _baseN / _baseD < e^2 (floorLog2 will return 1 if _baseN / _baseD < 4)
+        return 2;
+    if (scaledBaseN <= _baseD * 2008553): # _baseN / _baseD < e^3 (floorLog2 will return 2 if _baseN / _baseD < 8)
+        return 3;
+
+    return ((floorLog2((_baseN - 1) / _baseD) + 1) * 0xb17217f7d1cf78 + (1 << 56) - 1) >> 56; # ceiling(ceiling(log2(base)) * logE(2)))
 
 '''
     input range: 
@@ -58,8 +108,8 @@ def fixedLoge(_x, _precision):
     0xb17217f7d1cf78 = ln(2) * (1 << 56)
     
     '''
-    # Cannot represent negative numbers (below 1)
-    assert(_x >= (1) << _precision);
+    #Cannot represent negative numbers (below 1)
+    assert(_x >= ONE << _precision);
 
     log2 = fixedLog2(_x, _precision);
     return (log2 * 0xb17217f7d1cf78) >> 56;
@@ -69,7 +119,7 @@ def fixedLoge(_x, _precision):
     So x is assumed to be already upshifted 32 bits, and 
     the result is also upshifted 32 bits. 
     
-    [1] The def returns a number which is lower than the 
+    [1] The function returns a number which is lower than the 
     actual value
 
     input-range : 
@@ -81,24 +131,39 @@ def fixedLoge(_x, _precision):
 
 '''
 def fixedLog2(_x, _precision):
-    FIXED_ONE = (1) << _precision;
-    FIXED_TWO = (2) << _precision;
+    fixedOne = ONE << _precision;
+    fixedTwo = TWO << _precision;
 
     # Numbers below 1 are negative. 
-    assert( _x >= FIXED_ONE);
+    assert( _x >= fixedOne);
 
     hi = 0;
-    while (_x >= FIXED_TWO):
+    while (_x >= fixedTwo):
         _x >>= 1;
-        hi += FIXED_ONE;
+        hi += fixedOne;
 
     for i in range(_precision):
-        _x = (_x * _x) / FIXED_ONE;
-        if (_x >= FIXED_TWO):
+        _x = (_x * _x) / fixedOne;
+        if (_x >= fixedTwo):
             _x >>= 1;
-            hi += (1) << (_precision - 1 - i);
+            hi += ONE << (_precision - 1 - i);
 
     return hi;
+
+'''
+    floorLog2
+    Takes a natural number (n) as input.
+    Returns the largest integer smaller than or equal to the binary logarithm of the input.
+    Complexity is O(log(input bit - length)).
+'''
+def floorLog2(_n):
+    t = 0;
+    for s in [1<<(8-1-k) for k in range(8)]:
+        if (_n >= (ONE << s)):
+            _n >>= s;
+            t |= s;
+
+    return t;
 
 '''
     fixedExp is a 'protected' version of `fixedExpUnsafe`, which asserts instead of overflows.
@@ -129,8 +194,8 @@ def fixedLog2(_x, _precision):
     And in order to optimize for speed, we multiply MAX_FIXED_EXP_32 by 1.9^2 for every 2 additional precision units.
     Hence the general function for mapping a given precision to the maximum value permitted is:
     - precision = [32, 34, 36, ..., 62]
-    - MaxFixedExp(precision) = MAX_FIXED_EXP_32 * 3.61^(precision/2-16)
-    Since we cannot use non-integers, we do MAX_FIXED_EXP_32 * 361^(precision/2-16) / 100^(precision/2-16).
+    - MaxFixedExp(precision) = MAX_FIXED_EXP_32 * 3.61 ^ (precision / 2 - 16)
+    Since we cannot use non-integers, we do MAX_FIXED_EXP_32 * 361 ^ (precision / 2 - 16) / 100 ^ (precision / 2 - 16).
     But there is a better approximation, because this "1.9" factor in fact extends beyond a single decimal digit.
     So instead, we use 0xeb5ec5975959c565 / 0x4000000000000000, which yields maximum values quite close to real ones:
     maxExpArray = {
@@ -154,14 +219,14 @@ def fixedLog2(_x, _precision):
 '''
 def fixedExp(_x, _precision):
     maxExp = MAX_FIXED_EXP_32;
-    for p in range(32,_precision,2):
+    for p in range(32, _precision, 2):
         maxExp = (maxExp * 0xeb5ec5975959c565) >> (64-2);
     
     assert(_x <= maxExp);
     return fixedExpUnsafe(_x, _precision);
 
 '''
-    fixedExpUnsafe 
+    fixedExp 
     Calculates e ^ x according to maclauren summation:
 
     e^x = 1 + x + x ^ 2 / 2!...+ x ^ n / n!
@@ -169,15 +234,15 @@ def fixedExp(_x, _precision):
     and returns e ^ (x >> 32) << 32, that is, upshifted for accuracy
 
     Input range:
-        - def ok at    <= 242329958953 
-        - def fails at >= 242329958954
+        - Function ok at    <= 242329958953 
+        - Function fails at >= 242329958954
 
     This method is is visible for testcases, but not meant for direct use. 
 
     The values in this method been generated via the following python snippet: 
 
     def calculateFactorials():
-        """Method to print out the factorials for fixedExpUnsafe"""
+        """Method to print out the factorials for fixedExp"""
 
         ni = []
         ni.append(295232799039604140847618609643520000000) # 34!
@@ -189,7 +254,7 @@ def fixedExp(_x, _precision):
 '''
 def fixedExpUnsafe(_x, _precision):
     xi = _x;
-    res = 0xde1bc4d19efcac82445da75b00000000 << _precision;
+    res = (0xde1bc4d19efcac82445da75b00000000) << _precision;
 
     res += xi * 0xde1bc4d19efcac82445da75b00000000;
     xi = (xi * _x) >> _precision;
@@ -258,66 +323,6 @@ def fixedExpUnsafe(_x, _precision):
     res += xi * 0x22;
 
     return res / 0xde1bc4d19efcac82445da75b00000000;
-
-'''
-    getBestPrecision 
-    Predicts the highest precision which can be used in order to compute "base^exp" without exceeding 256 bits in any of the intermediate computations.
-    Instead of calculating "base^exp", we calculate "e^(ln(base)*exp)".
-    The value of ln(base) is represented with an integer slightly smaller than ln(base)*2^precision.
-    The larger the precision is, the more accurately this value represents the real value.
-    However, function fixedExpUnsafe(x), which calculates e^x, is limited to a maximum value of x.
-    The limit depends on the precision (e.g, for precision=32, the maximum value of x is MAX_FIXED_EXP_32).
-    Hence before calling the 'power' function, we need to estimate an upper-bound for ln(base)*exponent.
-    Of course, we should later assert that the value passed to fixedExpUnsafe is not larger than MAX_FIXED_EXP(precision).
-    Due to this assertion (made in function fixedExp), functions getBestPrecision and fixedExp are tightly coupled.
-    Note that the outcome of this function only affects the accuracy of the computation of "base^exp".
-    Therefore, there is no need to assert that no intermediate result exceeds 256 bits (nor in this function, neither in any of the functions down the calling tree).
-'''
-def getBestPrecision(_baseN, _baseD, _expN, _expD):
-    maxExp = MAX_FIXED_EXP_32;
-    maxVal = _expN * lnUpperBound(_baseN, _baseD);
-    for precision in range(32,64,2):
-        if (maxExp < (maxVal << precision) / _expD):
-            break;
-        maxExp = (maxExp * 0xeb5ec5975959c565) >> (64-2);
-    if (precision == 32):
-        return 32;
-    return precision-2;
-
-'''
-    lnUpperBound 
-    Takes a rational number (baseN/baseD) as input.
-    Returns an estimated upper-bound integer of the natural logarithm of the input.
-    We can generally use floorLog2, although there is possibly a tighter bound.
-    We cannot use floorLog2 when the input is smaller than 8.
-    Complexity is O(log(input bit-length)).
-'''
-def lnUpperBound(baseN, baseD):
-    assert(baseN > baseD);
-
-    scaled_baseN = baseN * 100000;
-    if (scaled_baseN <= baseD *  271828): # baseN / baseD < e^1 (floorLog2 will return 0 if baseN / baseD < 2)
-        return 1;
-    if (scaled_baseN <= baseD *  738905): # baseN / baseD < e^2 (floorLog2 will return 0 if baseN / baseD < 4)
-        return 2;
-    if (scaled_baseN <= baseD * 2008553): # baseN / baseD < e^3 (floorLog2 will return 0 if baseN / baseD < 8)
-        return 3;
-
-    return floorLog2(baseN/baseD);
-
-'''
-    floorLog2 
-    Takes a natural number (n) as input.
-    Returns the largest integer smaller than or equal to the binary logarithm of the input.
-    Complexity is O(log(input bit-length)).
-'''
-def floorLog2(n):
-    t = 0;
-    for k in range(7,-1,-1):
-        s = (n >= ((1) << (1 << k))) << k;
-        n >>= s;
-        t |= s;
-    return t;
 
 
 def safeMul(x,y):
