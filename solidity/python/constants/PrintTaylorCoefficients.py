6197b2ae82b9f705c4e9c2465f1b0c0b5e313ee8
barakman <barakman@yahoo.com>
2017-07-22 18:44:43 +0300

    Update PrintTaylorCoefficients.py

--- solidity/python/constants/PrintTaylorCoefficients.py
+++ solidity/python/constants/PrintTaylorCoefficients.py
@@ -5,7 +5,8 @@ NUM_OF_COEFS = 34
 
 
 maxFactorial = factorial(NUM_OF_COEFS)
+coefficients = [maxFactorial/factorial(i) for i in range(NUM_OF_COEFS)]
 
 
-for i in range(NUM_OF_COEFS):
-    print '0x{:x}'.format(maxFactorial/factorial(i))
+for coefficient in coefficients:
+    print '0x{:x}'.format(coefficient)
