e9ffd65facf21abf1cd48b7e96af6aa6e227ff1b
yudilevi <yudilevi@gmail.com>
2017-07-22 21:08:39 +0300

    added the BancorFormulaProxy contract

---
+++ solidity/contracts/BancorFormulaProxy.sol
@@ -0,0 +1,79 @@
+pragma solidity ^0.4.11;
+import './Owned.sol';
+import './interfaces/IBancorFormula.sol';
+
+/**
+    @dev the BancorFormulaProxy is an owned contract that serves as a single point of access
+    to the BancorFormula contract from all BancorChanger contract instances.
+    it allows upgrading the BancorFormula contract without the need to update each and every
+    BancorChanger contract instance individually.
+*/
+contract BancorFormulaProxy is IBancorFormula, Owned {
+    IBancorFormula public formula;  // bancor calculation formula contract
+
+    /**
+        @dev constructor
+
+        @param _formula address of a bancor formula contract
+    */
+    function BancorFormulaProxy(IBancorFormula _formula)
+        validAddress(_formula)
+    {
+        formula = _formula;
+    }
+
+    // validates an address - currently only checks that it isn't null
+    modifier validAddress(address _address) {
+        require(_address != 0x0);
+        _;
+    }
+
+    // verifies that the address is different than this contract address
+    modifier notThis(address _address) {
+        require(_address != address(this));
+        _;
+    }
+
+    /*
+        @dev allows the owner to update the formula contract address
+
+        @param _formula    address of a bancor formula contract
+    */
+    function setFormula(IBancorFormula _formula)
+        public
+        ownerOnly
+        validAddress(_formula)
+        notThis(_formula)
+    {
+        require(_formula != formula); // validate input
+        formula = _formula;
+    }
+
+    /**
+        @dev proxy for the bancor formula purchase return calculation
+
+        @param _supply             token total supply
+        @param _reserveBalance     total reserve
+        @param _reserveRatio       constant reserve ratio, 1-100
+        @param _depositAmount      deposit amount, in reserve token
+
+        @return purchase return amount
+    */
+    function calculatePurchaseReturn(uint256 _supply, uint256 _reserveBalance, uint8 _reserveRatio, uint256 _depositAmount) public constant returns (uint256) {
+        return formula.calculatePurchaseReturn(_supply, _reserveBalance, _reserveRatio, _depositAmount);
+     }
+
+    /**
+        @dev proxy for the bancor formula sale return calculation
+
+        @param _supply             token total supply
+        @param _reserveBalance     total reserve
+        @param _reserveRatio       constant reserve ratio, 1-100
+        @param _sellAmount         sell amount, in the token itself
+
+        @return sale return amount
+    */
+    function calculateSaleReturn(uint256 _supply, uint256 _reserveBalance, uint8 _reserveRatio, uint256 _sellAmount) public constant returns (uint256) {
+        return formula.calculateSaleReturn(_supply, _reserveBalance, _reserveRatio, _sellAmount);
+    }
+}
