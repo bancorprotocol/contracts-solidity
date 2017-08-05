6924f78afb1cb9245b4d8ea74b50ae9e7d0beb6f
Barak Manos <barakman@yahoo.com>
2017-08-05 19:41:24 +0300

    Cosmetic.

--- solidity/python/FormulaNativePython/__init__.py
+++ solidity/python/FormulaNativePython/__init__.py
@@ -5,17 +5,17 @@ from decimal import getcontext
 getcontext().prec = 80 # 78 digits for a maximum of 2^256-1, and 2 more digits for after the decimal point
 
 
-def calculatePurchaseReturn(_supply, _reserveBalance, _reserveRatio, _depositAmount):
-    return Decimal(_supply)*((1+Decimal(_depositAmount)/Decimal(_reserveBalance))**(Decimal(_reserveRatio)/100)-1)
+def calculatePurchaseReturn(supply, reserve, ratio, amount):
+    return Decimal(supply)*((1+Decimal(amount)/Decimal(reserve))**(Decimal(ratio)/100)-1)
 
 
-def calculateSaleReturn(_supply, _reserveBalance, _reserveRatio, _sellAmount):
-    return Decimal(_reserveBalance)*(1-(1-Decimal(_sellAmount)/Decimal(_supply))**(100/Decimal(_reserveRatio)))
+def calculateSaleReturn(supply, reserve, ratio, amount):
+    return Decimal(reserve)*(1-(1-Decimal(amount)/Decimal(supply))**(100/Decimal(ratio)))
 
 
-def power(_baseN, _baseD, _expN, _expD, _precision):
-    return (Decimal(_baseN)/Decimal(_baseD))**(Decimal(_expN)/Decimal(_expD))*2**_precision
+def power(baseN, baseD, expN, expD, precision):
+    return (Decimal(baseN)/Decimal(baseD))**(Decimal(expN)/Decimal(expD))*2**precision
 
 
-def ln(_numerator, _denominator, _precision):
-    return (Decimal(_numerator)/Decimal(_denominator)).ln()*2**_precision
+def ln(numerator, denominator, precision):
+    return (Decimal(numerator)/Decimal(denominator)).ln()*2**precision
