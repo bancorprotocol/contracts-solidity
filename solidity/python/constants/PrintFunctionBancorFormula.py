90caa211657ce1b16ab84332a8355b2932691a7c
barak manos <barakman@yahoo.com>
2017-08-05 20:58:08 +0300

    Cosmetic.

--- solidity/python/constants/PrintFunctionBancorFormula.py
+++ solidity/python/constants/PrintFunctionBancorFormula.py
@@ -8,9 +8,13 @@ MAX_PRECISION = 127
 maxExpArray = getMaxExpArray(MAX_PRECISION+1)
 
 
+len1 = len('{:d}'.format(MAX_PRECISION))
+len2 = len('0x{:x}'.format(maxExpArray[0]<<MAX_PRECISION))
+
+
 print '    uint256[{}] maxExpArray;'.format(len(maxExpArray))
 print '    function BancorFormula() {'
 for precision in range(len(maxExpArray)):
     prefix = '  ' if MIN_PRECISION <= precision <= MAX_PRECISION else '//'
-    print '    {}  maxExpArray[{:3d}] = 0x{:x};'.format(prefix,precision,maxExpArray[precision]<<(MAX_PRECISION-precision))
+    print '    {0:s}  maxExpArray[{1:{2}d}] = {3:#0{4}x};'.format(prefix,precision,len1,maxExpArray[precision]<<(MAX_PRECISION-precision),len2)
 print '    }'
