819b6f33b0e482f59773407ea452fc3c80126d57
barakman <barakman@yahoo.com>
2017-08-06 07:33:15 +0300

    Increase the precision that maxExpArray yields.

--- solidity/python/constants/PrintFunctionBancorFormula.py
+++ solidity/python/constants/PrintFunctionBancorFormula.py
@@ -8,13 +8,17 @@ MAX_PRECISION = 127
 maxExpArray = getMaxExpArray(MAX_PRECISION+1)
 
 
+def maxExpArrayShl(precision):
+    return ((maxExpArray[precision]+1)<<(MAX_PRECISION-precision))-1
+
+
 len1 = len('{:d}'.format(MAX_PRECISION))
-len2 = len('0x{:x}'.format(maxExpArray[0]<<MAX_PRECISION))
+len2 = len('0x{:x}'.format(maxExpArrayShl(0)))
 
 
 print '    uint256[{}] maxExpArray;'.format(len(maxExpArray))
 print '    function BancorFormula() {'
 for precision in range(len(maxExpArray)):
     prefix = '  ' if MIN_PRECISION <= precision <= MAX_PRECISION else '//'
-    print '    {0:s}  maxExpArray[{1:{2}d}] = {3:#0{4}x};'.format(prefix,precision,len1,maxExpArray[precision]<<(MAX_PRECISION-precision),len2)
+    print '    {0:s}  maxExpArray[{1:{2}d}] = {3:#0{4}x};'.format(prefix,precision,len1,maxExpArrayShl(precision),len2)
 print '    }'
