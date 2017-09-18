8c998b05ea1da1f8286d624966fd33493dc6d89f
Dev43 <patrick.guay43@gmail.com>
2017-09-18 15:26:52 -0400

    reorganize repo structure

--- solidity/contracts/interfaces/IBancorFormula.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.4.11;
-
-/*
-    Bancor Formula interface
-*/
-contract IBancorFormula {
-    function calculatePurchaseReturn(uint256 _supply, uint256 _reserveBalance, uint8 _reserveRatio, uint256 _depositAmount) public constant returns (uint256);
-    function calculateSaleReturn(uint256 _supply, uint256 _reserveBalance, uint8 _reserveRatio, uint256 _sellAmount) public constant returns (uint256);
-}
--- solidity/migrations/2_deploy_contracts.js
+++ solidity/migrations/2_deploy_contracts.js
@@ -1,30 +1,11 @@
 /* global artifacts */
 /* eslint-disable prefer-reflect */
-
-const Utils = artifacts.require('Utils.sol');
-const Owned = artifacts.require('Owned.sol');
-const Managed = artifacts.require('Managed.sol');
-const TokenHolder = artifacts.require('TokenHolder.sol');
-const ERC20Token = artifacts.require('ERC20Token.sol');
-const EtherToken = artifacts.require('EtherToken.sol');
-const SmartToken = artifacts.require('SmartToken.sol');
-const SmartTokenController = artifacts.require('SmartTokenController.sol');
-const BancorFormula = artifacts.require('BancorFormula.sol');
-const BancorFormulaProxy = artifacts.require('BancorFormulaProxy.sol');
-const BancorChanger = artifacts.require('BancorChanger.sol');
-const CrowdsaleController = artifacts.require('CrowdsaleController.sol');
+const bigNumber = require('bignumber.js');
+const ENJCrowdfund = artifacts.require('ENJCrowdfund.sol');
+const ENJToken = artifacts.require('ENJToken.sol');
 
 module.exports = async (deployer) => {
-    deployer.deploy(Utils);
-    deployer.deploy(Owned);
-    deployer.deploy(Managed);
-    deployer.deploy(TokenHolder);
-    deployer.deploy(ERC20Token, 'DummyToken', 'DUM', 0);
-    deployer.deploy(EtherToken);
-    await deployer.deploy(SmartToken, 'Token1', 'TKN1', 2);
-    deployer.deploy(SmartTokenController, SmartToken.address);
-    deployer.deploy(BancorFormula);
-    deployer.deploy(BancorFormulaProxy, '0x125463');
-    deployer.deploy(BancorChanger, SmartToken.address, '0x124', 0, '0x0', 0);
-    deployer.deploy(CrowdsaleController, SmartToken.address, 4102444800, '0x125', '0x126', 1);
+    deployer.deploy(ENJCrowdfund, new bigNumber(4).times(new bigNumber(10).pow(26)), this.web3.eth.accounts[1]).then(() => {
+        deployer.deploy(ENJToken, ENJCrowdfund.address, this.web3.eth.accounts[2]);
+    })
 };
