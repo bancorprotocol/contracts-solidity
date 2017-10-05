9bc69b05db1d6266a19c288011b9d8703d3f2e87
barakman <barakman@yahoo.com>
2017-10-05 13:36:12 +0300

    Add Python tests.

---
+++ solidity/python/CoverageUniTestSale.py
@@ -0,0 +1,94 @@
+import InputGenerator
+import FormulaSolidityPort
+import FormulaNativePython
+
+
+MINIMUM_VALUE_SUPPLY  = 100
+MAXIMUM_VALUE_SUPPLY  = 10**34
+SAMPLES_COUNT_SUPPLY  = 150
+
+
+MINIMUM_VALUE_RESERVE = 100
+MAXIMUM_VALUE_RESERVE = 10**34
+SAMPLES_COUNT_RESERVE = 150
+
+
+MINIMUM_VALUE_RATIO   = 100000
+MAXIMUM_VALUE_RATIO   = 900000
+SAMPLES_COUNT_RATIO   = 10
+
+
+MINIMUM_VALUE_AMOUNT  = 1
+MAXIMUM_VALUE_AMOUNT  = 10**34
+SAMPLES_COUNT_AMOUNT  = 150
+
+
+def Main():    
+    range_supply  = InputGenerator.UniformDistribution(MINIMUM_VALUE_SUPPLY ,MAXIMUM_VALUE_SUPPLY ,SAMPLES_COUNT_SUPPLY )
+    range_reserve = InputGenerator.UniformDistribution(MINIMUM_VALUE_RESERVE,MAXIMUM_VALUE_RESERVE,SAMPLES_COUNT_RESERVE)
+    range_ratio   = InputGenerator.UniformDistribution(MINIMUM_VALUE_RATIO  ,MAXIMUM_VALUE_RATIO  ,SAMPLES_COUNT_RATIO  )
+    range_amount  = InputGenerator.UniformDistribution(MINIMUM_VALUE_AMOUNT ,MAXIMUM_VALUE_AMOUNT ,SAMPLES_COUNT_AMOUNT )
+    
+    testNum = 0
+    numOfTests = len(range_supply)*len(range_reserve)*len(range_ratio)*len(range_amount)
+    
+    worstAbsoluteLoss = Record(range_supply[0],range_reserve[0],range_ratio[0],range_amount[0],0,0.0)
+    worstRelativeLoss = Record(range_supply[0],range_reserve[0],range_ratio[0],range_amount[0],0,0.0)
+    
+    failureTransactionCount = 0
+    invalidTransactionCount = 0
+    
+    try:
+        for             supply  in range_supply :
+            for         reserve in range_reserve:
+                for     ratio   in range_ratio  :
+                    for amount  in range_amount :
+                        testNum += 1
+                        if amount <= supply:
+                            resultSolidityPort = Run(FormulaSolidityPort,supply,reserve,ratio,amount)
+                            resultNativePython = Run(FormulaNativePython,supply,reserve,ratio,amount)
+                            if resultNativePython < 0:
+                                invalidTransactionCount += 1
+                            elif resultSolidityPort < 0:
+                                failureTransactionCount += 1
+                            elif resultNativePython < resultSolidityPort:
+                                print 'Implementation Error:',Record(supply,reserve,ratio,amount,resultSolidityPort,resultNativePython)
+                                return
+                            else: # 0 <= resultSolidityPort <= resultNativePython
+                                absoluteLoss = resultNativePython-resultSolidityPort
+                                relativeLoss = 1-resultSolidityPort/resultNativePython
+                                worstAbsoluteLoss.Update(supply,reserve,ratio,amount,resultSolidityPort,resultNativePython,absoluteLoss,relativeLoss)
+                                worstRelativeLoss.Update(supply,reserve,ratio,amount,resultSolidityPort,resultNativePython,relativeLoss,absoluteLoss)
+                                worstAbsoluteLossStr = 'worstAbsoluteLoss = {:.0f} (relativeLoss = {:.0f}%)'.format(worstAbsoluteLoss.major,worstAbsoluteLoss.minor*100)
+                                worstRelativeLossStr = 'worstRelativeLoss = {:.0f}% (absoluteLoss = {:.0f})'.format(worstRelativeLoss.major*100,worstRelativeLoss.minor)
+                                print 'Test {} out of {}: {}, {}'.format(testNum,numOfTests,worstAbsoluteLossStr,worstRelativeLossStr)
+    except KeyboardInterrupt:
+        print 'Process aborted by user request'
+    
+    print 'worstAbsoluteLoss:',worstAbsoluteLoss
+    print 'worstRelativeLoss:',worstRelativeLoss
+    
+    print 'failureTransactionCount:',failureTransactionCount
+    print 'invalidTransactionCount:',invalidTransactionCount
+
+
+def Run(module,supply,reserve,ratio,amount):
+    try:
+        return module.calculateSaleReturn(supply,reserve,ratio,amount)
+    except Exception:
+        return -1
+
+
+class Record():
+    def __init__(self,supply,reserve,ratio,amount,resultSolidityPort,resultNativePython,major=0.0,minor=0.0):
+        self._set(supply,reserve,ratio,amount,resultSolidityPort,resultNativePython,major,minor)
+    def __str__(self):
+        return ''.join(['\n\t{} = {}'.format(var,vars(self)[var]) for var in 'supply,reserve,ratio,amount,resultSolidityPort,resultNativePython'.split(',')])
+    def Update(self,supply,reserve,ratio,amount,resultSolidityPort,resultNativePython,major,minor):
+        if self.major < major or (self.major == major and self.minor < minor):
+            self._set(supply,reserve,ratio,amount,resultSolidityPort,resultNativePython,major,minor)
+    def _set(self,supply,reserve,ratio,amount,resultSolidityPort,resultNativePython,major,minor):
+        self.__dict__.update({key:val for key,val in locals().iteritems() if key != 'self'})
+
+
+Main()
---
+++ solidity/python/EmulationUniTestSale.py
@@ -0,0 +1,67 @@
+import web3
+import json
+import InputGenerator
+import FormulaSolidityPort
+
+
+MINIMUM_VALUE_SUPPLY  = 100
+MAXIMUM_VALUE_SUPPLY  = 10**34
+SAMPLES_COUNT_SUPPLY  = 150
+
+
+MINIMUM_VALUE_RESERVE = 100
+MAXIMUM_VALUE_RESERVE = 10**34
+SAMPLES_COUNT_RESERVE = 150
+
+
+MINIMUM_VALUE_RATIO   = 100000
+MAXIMUM_VALUE_RATIO   = 900000
+SAMPLES_COUNT_RATIO   = 10
+
+
+MINIMUM_VALUE_AMOUNT  = 1
+MAXIMUM_VALUE_AMOUNT  = 10**34
+SAMPLES_COUNT_AMOUNT  = 150
+
+
+def Main():    
+    range_supply  = InputGenerator.UniformDistribution(MINIMUM_VALUE_SUPPLY ,MAXIMUM_VALUE_SUPPLY ,SAMPLES_COUNT_SUPPLY )
+    range_reserve = InputGenerator.UniformDistribution(MINIMUM_VALUE_RESERVE,MAXIMUM_VALUE_RESERVE,SAMPLES_COUNT_RESERVE)
+    range_ratio   = InputGenerator.UniformDistribution(MINIMUM_VALUE_RATIO  ,MAXIMUM_VALUE_RATIO  ,SAMPLES_COUNT_RATIO  )
+    range_amount  = InputGenerator.UniformDistribution(MINIMUM_VALUE_AMOUNT ,MAXIMUM_VALUE_AMOUNT ,SAMPLES_COUNT_AMOUNT )
+    
+    testNum = 0
+    numOfTests = len(range_supply)*len(range_reserve)*len(range_ratio)*len(range_amount)
+    
+    web3RPCProvider = web3.Web3(web3.RPCProvider())
+    abi = open('../contracts/build/BancorFormula.abi').read()
+    bin = open('../contracts/build/BancorFormula.bin').read()
+    contract = web3RPCProvider.eth.contract(abi=json.loads(abi),bytecode=bin)
+    FormulaContractAddr = contract(web3RPCProvider.eth.getTransactionReceipt(contract.deploy())['contractAddress']).call()
+    
+    for             supply  in range_supply :
+        for         reserve in range_reserve:
+            for     ratio   in range_ratio  :
+                for amount  in range_amount :
+                    testNum += 1
+                    if amount <= supply:
+                        resultSolidityPort = Run(FormulaSolidityPort,supply,reserve,ratio,amount)
+                        resultContractAddr = Run(FormulaContractAddr,supply,reserve,ratio,amount)
+                        print 'Test {} out of {}: resultSolidityPort = {}, resultContractAddr = {}'.format(testNum,numOfTests,resultSolidityPort,resultContractAddr)
+                        if resultSolidityPort != resultContractAddr:
+                            print 'Emulation Error:'
+                            print 'supply  = {}'.format(supply )
+                            print 'reserve = {}'.format(reserve)
+                            print 'ratio   = {}'.format(ratio  )
+                            print 'amount  = {}'.format(amount )
+                            return
+
+
+def Run(module,supply,reserve,ratio,amount):
+    try:
+        return module.calculateSaleReturn(supply,reserve,ratio,amount)
+    except Exception:
+        return -1
+
+
+Main()
