4307282fb94f67894931997fee4443af287d3bbc
barakman <barakman@yahoo.com>
2017-10-15 11:09:36 +0300

    Converter module.

--- solidity/python/converter/run.py
+++ solidity/python/converter/run.py
@@ -2,4 +2,4 @@ from sys    import argv
 from engine import Engine
 
 
-Engine.run(argv[1],argv[2]) if len(argv) > 2 else Engine.run('database.json','commands.json')
+Engine.run(argv[1],argv[2]) if len(argv) > 2 else Engine.run('example_database.json','example_commands.json')
