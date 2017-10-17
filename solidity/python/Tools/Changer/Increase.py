603c4c89f455331e14b756811bd98aea92f36e5b
barakman <barakman@yahoo.com>
2017-10-17 08:54:57 +0300

    Add tools for computing: - The amount that you need to buy in order to increase the price by a given percentage - The amount that you need to sell in order to decrease the price by a given percentage

---
+++ solidity/python/Tools/Changer/Increase.py
@@ -0,0 +1,33 @@
+from sys     import argv
+from os.path import basename
+from decimal import Decimal
+from decimal import getcontext
+
+
+getcontext().prec = 30
+
+
+def Main(supply,balance,ratio,change):
+    ratio  /= 100
+    change /= 100
+    cur_amount = balance*((1+change)**(1/(1-ratio))-1)
+    new_amount = supply*((1+cur_amount/balance)**ratio-1)
+    cur_price = balance/(supply*ratio)
+    new_price = (balance+cur_amount)/((supply+new_amount)*ratio)
+    print 'At present:'
+    print '- The supply  = {:.20f}'.format(supply)
+    print '- The balance = {:.20f}'.format(balance)
+    print '- The price   = {:.20f}'.format(cur_price)
+    print 'If you buy an amount of {:.20f}, then:'.format(cur_amount)
+    print '- The supply  = {:.20f}'.format(supply+new_amount)
+    print '- The balance = {:.20f}'.format(balance+cur_amount)
+    print '- The price   = {:.20f}'.format(new_price)
+    print 'Which reflects a price increase of {:.20f} percent'.format((new_price-cur_price)/cur_price*100)
+
+
+if len(argv) == 5:
+    supply,balance,ratio,change = [Decimal(arg) for arg in argv[1:]]
+    assert(0 < supply and 0 < balance and 0 < ratio <= 100 and 0 <= change)
+    Main(supply,balance,ratio,change)
+else:
+    print '{} <supply> <balance> <ratio> <desired price change>'.format(basename(__file__))
