a571cdcda62b37fc04da221457d973a60d7ad468
barakman <barakman@yahoo.com>
2017-10-20 19:07:21 +0300

    Converter module: Save all data in an Excel (CSV) file.

--- solidity/python/Utilities/Converter/run.py
+++ solidity/python/Utilities/Converter/run.py
@@ -5,6 +5,7 @@ from engine import Engine
 
 
 store  = {}
+report = []
 engine = Engine()
 
 
@@ -39,13 +40,32 @@ def main():
             if case == [True ,True ]: print 'Cannot convert unspecified amount to unspecified amount'
         else:
             print 'Undefined operation'
+    fileName = argv[2] if len(argv) > 2 else 'example_report.csv'
+    fileDesc = open(fileName,'w')
+    fileDesc.write(report2csv())
+    fileDesc.close()
 
 
 def convert(sign,source,target,input,output,update):
     entries = engine.convert(sign,source,target,store[input] if input in store else input,update)
+    report.append(entries)
     if output:
         store[output] = entries[-(sign+1)/2]['amount']
     return ' = '.join(['{:.2f} {}'.format(entry['amount'],entry['currency']) for entry in entries])
+
+
+def report2csv():
+    rows = ['Source Amount,Source Token,Target Amount,Target Token,Rate,Supply Before,Balance Before,Supply After,Balance After']
+    for entries in report:
+        for first,second in [(entries[0],entries[-1])]:
+            rows += ['{:.2f},{},{:.2f},{},{:.2f},,,,'.format(first['amount'],first['currency'],second['amount'],second['currency'],first['amount']/second['amount'])]
+        for first,second in zip(entries,entries[1:]):
+            rows += ['{:.2f},{},{:.2f},{},,{:.2f},{:.2f},{:.2f},{:.2f}'.format(first['amount'],first['currency'],second['amount'],second['currency'],first['supply'],first['balance'],second['supply'],second['balance'])]
+    maxLens = [len(col) for col in rows[0].split(',')]
+    for row in rows[1:]:
+        maxLens = [max(first,second) for first,second in zip(maxLens,[len(col) for col in row.split(',')])]
+    formatStrings = ['{}{}{}'.format('{:',maxLen,'s}') for maxLen in maxLens]
+    return '\n'.join([' , '.join([first.format(second) for first,second in zip(formatStrings,row.split(','))]) for row in rows])+'\n'
 
 
 main()
