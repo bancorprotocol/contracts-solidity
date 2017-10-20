e54c21b93f9409fb6eff1642cd436beaf569c923
barakman <barakman@yahoo.com>
2017-10-20 10:42:20 +0300

    Minor change in the Converter module (preliminary work for the purpose of generating a CSV file containing all conversions).

--- solidity/python/Utilities/Converter/run.py
+++ solidity/python/Utilities/Converter/run.py
@@ -4,15 +4,16 @@ from json   import dumps
 from engine import Engine
 
 
+store  = {}
+engine = Engine()
+
+
 def main():
     fileName = argv[1] if len(argv) > 1 else 'example_commands.json'
     fileDesc = open(fileName)
     fileData = fileDesc.read()
     fileDesc.close()
-    store    = {}
-    engine   = Engine()
-    commands = loads(fileData)
-    for command in commands:
+    for command in loads(fileData):
         if command['operation'] == 'print':
             print command['info']
         elif command['operation'] == 'load':
@@ -33,14 +34,14 @@ def main():
             args = command['line'].split()
             case = [all(c == '?' for c in args[n]) for n in [0,3]]
             if case == [False,False]: print 'Cannot convert specified amount to specified amount'
-            if case == [False,True ]: print 'Explicit:',convert(engine,+1,args[1],args[4],args[0],command['result'],command['update'],store)
-            if case == [True ,False]: print 'Implicit:',convert(engine,-1,args[1],args[4],args[3],command['result'],command['update'],store)
+            if case == [False,True ]: print 'Explicit:',convert(+1,args[1],args[4],args[0],command['result'],command['update'])
+            if case == [True ,False]: print 'Implicit:',convert(-1,args[1],args[4],args[3],command['result'],command['update'])
             if case == [True ,True ]: print 'Cannot convert unspecified amount to unspecified amount'
         else:
             print 'Undefined operation'
 
 
-def convert(engine,sign,source,target,input,output,update,store):
+def convert(sign,source,target,input,output,update):
     entries = engine.convert(sign,source,target,store[input] if input in store else input,update)
     if output:
         store[output] = entries[-(sign+1)/2]['amount']
