7b06319b72c0860a54bbef5971f40ca626b835b7
Or <or.dd100@gmail.com>
2017-12-05 16:16:49 +0200

    removed vm exception

--- solidity/test/helpers/Utils.js
+++ solidity/test/helpers/Utils.js
@@ -2,7 +2,7 @@
 
 function isException(error) {
     let strError = error.toString();
-    return strError.includes('invalid opcode') || strError.includes('invalid JUMP') || strError.includes('VM Exception');
+    return strError.includes('invalid opcode') || strError.includes('invalid JUMP');
 }
 
 function ensureException(error) {
