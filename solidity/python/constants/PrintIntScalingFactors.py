6d8be7b0a636e0ff93b1812c350131e8a1e354d4
barakman <barakman@yahoo.com>
2017-12-31 15:16:44 +0200

    Fix "compilation" error made in previous commit.

--- solidity/python/constants/PrintIntScalingFactors.py
+++ solidity/python/constants/PrintIntScalingFactors.py
@@ -6,7 +6,7 @@ FIXED_2 = 2<<MAX_PRECISION
 MAX_NUM = (1<<(256-MAX_PRECISION))-1
 
 
-maxLen = len(hex(max[FIXED_1,FIXED_2,MAX_NUM]))
+maxLen = len(hex(max([FIXED_1,FIXED_2,MAX_NUM])))
 
 
 print '    uint256 private constant FIXED_1 = {0:#0{1}x};'.format(FIXED_1,maxLen)
