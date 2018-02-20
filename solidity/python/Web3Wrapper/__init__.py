b0ae9fc745ee8ae502e7b8fa35e8a8bfc5726e44
barakman <barakman@yahoo.com>
2018-02-20 08:45:55 +0200

    Minor fix in Web3Wrapper.

--- solidity/python/Web3Wrapper/__init__.py
+++ solidity/python/Web3Wrapper/__init__.py
@@ -3,15 +3,17 @@ from web3 import RPCProvider
 from json import loads
 
 
+eth = Web3(RPCProvider()).eth
+
+
 class Contract():
-    eth = Web3(RPCProvider()).eth
     def __init__(self,moduleName,ownerAddress='',args=[]):
         path = '../../solidity/contracts/build/'
         abi = open(path+moduleName+'.abi').read()
         bin = open(path+moduleName+'.bin').read()
-        self.contract = Contract.eth.contract(abi=loads(abi),bytecode=bin)
-        self.transact = {'from':ownerAddress if ownerAddress else Contract.eth.accounts[0]}
-        self.address  = Contract.eth.getTransactionReceipt(self.contract.deploy(transaction=self.transact,args=args))['contractAddress']
+        self.contract = eth.contract(abi=loads(abi),bytecode=bin)
+        self.transact = {'from':ownerAddress if ownerAddress else eth.accounts[0]}
+        self.address  = eth.getTransactionReceipt(self.contract.deploy(transaction=self.transact,args=args))['contractAddress']
     def owner(self):
         return self.transact['from']
     def getter(self):
@@ -20,5 +22,5 @@ class Contract():
         return self.contract(self.address).transact(self.transact)
     def tester(self):
         return self.contract(self.address).estimateGas(self.transact)
-    def retval(self,hash):
-        return int(Contract.eth.getTransactionReceipt(hash)['logs'][-1]['data'],0)
+    def retval(hash):
+        return int(eth.getTransactionReceipt(hash)['logs'][-1]['data'],0)
