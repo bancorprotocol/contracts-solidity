2960f479e4b443be121954a11dc8b073bc31c272
barakman <barakman@yahoo.com>
2018-02-26 15:17:36 +0200

    Update the auxiliary contract TestBancorFormula.

--- solidity/contracts/helpers/TestBancorFormula.sol
+++ solidity/contracts/helpers/TestBancorFormula.sol
@@ -8,23 +8,35 @@ contract TestBancorFormula is BancorFormula {
     function TestBancorFormula() public {
     }
 
-    function powerTest(uint256 _baseN, uint256 _baseD, uint32 _expN, uint32 _expD) public constant returns (uint256, uint8) {
+    function powerTest(uint256 _baseN, uint256 _baseD, uint32 _expN, uint32 _expD) external view returns (uint256, uint8) {
         return super.power(_baseN, _baseD, _expN, _expD);
     }
 
-    function lnTest(uint256 _numerator, uint256 _denominator) public pure returns (uint256) {
+    function lnTest(uint256 _numerator, uint256 _denominator) external pure returns (uint256) {
         return super.ln(_numerator, _denominator);
     }
 
-    function findPositionInMaxExpArrayTest(uint256 _x) public constant returns (uint8) {
+    function findPositionInMaxExpArrayTest(uint256 _x) external view returns (uint8) {
         return super.findPositionInMaxExpArray(_x);
     }
 
-    function fixedExpTest(uint256 _x, uint8 _precision) public pure returns (uint256) {
+    function fixedExpTest(uint256 _x, uint8 _precision) external pure returns (uint256) {
         return super.fixedExp(_x, _precision);
     }
 
-    function floorLog2Test(uint256 _n) public pure returns (uint8) {
+    function floorLog2Test(uint256 _n) external pure returns (uint8) {
         return super.floorLog2(_n);
+    }
+
+    function powTest(uint256 a, uint256 b, uint256 c, uint256 d) external pure returns (uint256) {
+        return super.pow(a, b, c, d);
+    }
+
+    function logTest(uint256 x) external pure returns (uint256) {
+        return super.log(x);
+    }
+
+    function expTest(uint256 x) external pure returns (uint256) {
+        return super.exp(x);
     }
 }
