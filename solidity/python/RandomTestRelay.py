05bcef735af2cae4eab437f72b6a25c3f6beeb1d
barakman <barakman@yahoo.com>
2018-03-06 00:15:20 +0200

    Update RandomTestRelay.py.

--- solidity/python/RandomTestRelay.py
+++ solidity/python/RandomTestRelay.py
@@ -3,11 +3,16 @@ import random
 import FormulaSolidityPort
 
 
+from decimal import Decimal
+from decimal import getcontext
+getcontext().prec = 100
+
+
 def formulaTest(supply, balance1, weight1, balance2, weight2, amount):
     singleHopResult = FormulaSolidityPort.calculateRelayReturn(balance1, weight1, balance2, weight2, amount)
     newAmount       = FormulaSolidityPort.calculatePurchaseReturn(supply, balance1, weight1, amount)
     doubleHopResult = FormulaSolidityPort.calculateSaleReturn(supply + newAmount, balance2, weight2, newAmount)
-    return singleHopResult, doubleHopResult
+    return Decimal(singleHopResult) / Decimal(doubleHopResult)
 
 
 size = int(sys.argv[1]) if len(sys.argv) > 1 else 0
@@ -15,6 +20,8 @@ if size == 0:
     size = int(input('How many test-cases would you like to execute? '))
 
 
+minRatio = Decimal('+inf')
+maxRatio = Decimal('-inf')
 numOfFailures = 0
 
 
@@ -26,7 +33,9 @@ for n in range(size):
     weight2 = random.randrange(1, 1000000)
     amount = random.randrange(1, supply)
     try:
-        singleHopResult, doubleHopResult = formulaTest(supply, balance1, weight1, balance2, weight2, amount)
+        ratio = formulaTest(supply, balance1, weight1, balance2, weight2, amount)
+        minRatio = min(minRatio, ratio)
+        maxRatio = max(maxRatio, ratio)
     except Exception as error:
         numOfFailures += 1
-    print('Test #{}: singleHopResult = {}, doubleHopResult = {}, num of failures = {}'.format(n, singleHopResult, doubleHopResult, numOfFailures))
+    print('Test #{}: ratio = {:.12f}, minRatio = {:.12f}, maxRatio = {:.12f}, num of failures = {}'.format(n, ratio, minRatio, maxRatio, numOfFailures))
