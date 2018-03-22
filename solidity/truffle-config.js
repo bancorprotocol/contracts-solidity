9145d65d85c63d239cb99a81dc12794e94244db3
Yudi Levi <yudi@localcoin.com>
2018-03-22 20:36:49 +0200

    updated configuration

--- solidity/truffle-config.js
+++ solidity/truffle-config.js
@@ -5,7 +5,7 @@ module.exports = {
             port: 8545,
             network_id: "*", // Match any network id
             gasPrice: 22000000000,
-            gas: 6000000
+            gas: 4712388
         }
     },
     solc: {
