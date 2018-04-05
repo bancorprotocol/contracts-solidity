6563bb89de5cc909b0dc24fc47c431e4036dd6da
detoo <daily.detoo@gmail.com>
2018-04-05 00:41:24 -0700

    Fix validateGasPrice does not allow lower prices.

--- solidity/contracts/BancorGasPriceLimit.sol
+++ solidity/contracts/BancorGasPriceLimit.sol
@@ -58,6 +58,6 @@ contract BancorGasPriceLimit is IBancorGasPriceLimit, Owned, Utils {
         view
         greaterThanZero(_gasPrice)
     {
-        require(_gasPrice == gasPrice);
+        require(_gasPrice <= gasPrice);
     }
 }
