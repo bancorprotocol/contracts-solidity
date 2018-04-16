e39d1393a8fc4957906184df8b82b89066d58154
Or <ord@bancor.network>
2018-04-16 16:24:44 +0300

    Minor change.

--- solidity/contracts/BancorQuickConverter.sol
+++ solidity/contracts/BancorQuickConverter.sol
@@ -125,11 +125,7 @@ contract BancorQuickConverter is IBancorQuickConverter, TokenHolder {
 
         @return tokens issued in return
     */
-    function convertFor(IERC20Token[] _path, uint256 _amount, uint256 _minReturn, address _for)
-        public
-        payable
-        returns (uint256)
-    {
+    function convertFor(IERC20Token[] _path, uint256 _amount, uint256 _minReturn, address _for) public payable returns (uint256) {
         return convertForPrioritized(_path, _amount, _minReturn, _for, 0x0, 0x0, 0x0, 0x0, 0x0);
     }
 
