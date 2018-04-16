1db8c255b951a1f9d41fa40c4def2afc16876ab6
Or <ord@bancor.network>
2018-04-16 14:22:22 +0300

    Minor changes.

--- solidity/contracts/interfaces/IBancorQuickConverter.sol
+++ solidity/contracts/interfaces/IBancorQuickConverter.sol
@@ -6,8 +6,7 @@ import './IBancorGasPriceLimit.sol';
     Bancor Quick Converter interface
 */
 contract IBancorQuickConverter {
-    function signerAddress() public view returns (address);
     function convert(IERC20Token[] _path, uint256 _amount, uint256 _minReturn) public payable returns (uint256);
     function convertFor(IERC20Token[] _path, uint256 _amount, uint256 _minReturn, address _for) public payable returns (uint256);
-    function prioritizeConvertFor(IERC20Token[] _path, uint256 _amount, uint256 _minReturn, address _for, uint256 _nonce, uint8 _v, bytes32 _r, bytes32 _s) public payable returns (uint256);
+    function convertForPrioritized(IERC20Token[] _path, uint256 _amount, uint256 _minReturn, address _for, uint256 _block, uint256 _nonce, uint8 _v, bytes32 _r, bytes32 _s) public payable returns (uint256);
 }
