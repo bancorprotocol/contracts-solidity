b1a61d30aad11f2734c365116c30d1db8e7c3ae4
Yudi Levi <yudilevi@users.noreply.github.com>
2018-04-16 16:29:30 +0300

    Update BancorQuickConverter.sol

--- solidity/contracts/BancorQuickConverter.sol
+++ solidity/contracts/BancorQuickConverter.sol
@@ -95,19 +95,19 @@ contract BancorQuickConverter is IBancorQuickConverter, TokenHolder {
     function verifyTrustedSender(uint256 _block, address _addr, uint256 _nonce, uint8 _v, bytes32 _r, bytes32 _s) private returns(bool) {
         bytes32 hash = sha256(_block, tx.gasprice, _addr, _nonce);
 
-        // checking that it is first time conversion with the given signature
-        // and that the current block number not exceeded the maximal block
-        // number that allowed for the current signature
+        // checking that it is the first conversion with the given signature
+        // and that the current block number doesn't exceeded the maximum block
+        // number that's allowed with the current signature
         require(!conversionHashes[hash] && block.number <= _block);
 
-        // recover the signing address and compare it to the trusted signer
+        // recovering the signing address and comparing it to the trusted signer
         // address that was set in the contract
         bytes memory prefix = "\x19Ethereum Signed Message:\n32";
         bytes32 prefixedHash = keccak256(prefix, hash);
         bool verified = ecrecover(prefixedHash, _v, _r, _s) == signerAddress;
 
-        // in case the signer is a trusted entity - mark the signature since
-        // every valid signature is valid only for one time use
+        // if the signer is the trusted signer - mark the hash so that it can't
+        // be used multiple times
         if (verified)
             conversionHashes[hash] = true;
         return verified;
