a27c284762ebc20ca08c0f476264ef6d4747338f
barakman <barakman@yahoo.com>
2018-05-21 19:59:38 +0300

    Extend the RandomTestCrossConnector.py test to ensure that in terms of accuracy, SaleFixed(PurchaseFixed(x)) <= CrossConnectorFixed(x) <= SaleFloat(PurchaseFloat(x)).

--- solidity/python/RandomTestCrossConnector.py
+++ solidity/python/RandomTestCrossConnector.py
@@ -9,16 +9,27 @@ from decimal import getcontext
 getcontext().prec = 80 # 78 digits for a maximum of 2^256-1, and 2 more digits for after the decimal point
 
 
-def singleHopTest(balance1, weight1, balance2, weight2, amount):
+def singleHopTestFixed(balance1, weight1, balance2, weight2, amount):
     try:
         return FormulaSolidityPort.calculateCrossConnectorReturn(balance1, weight1, balance2, weight2, amount)
     except:
         return -1
 
 
-def doubleHopTest(supply, balance1, weight1, balance2, weight2, amount):
-    amount = FormulaNativePython.calculatePurchaseReturn(supply, balance1, weight1, amount)
-    return FormulaNativePython.calculateSaleReturn(supply + amount, balance2, weight2, amount)
+def doubleHopTestFixed(supply, balance1, weight1, balance2, weight2, amount):
+    try:
+        amount = FormulaSolidityPort.calculatePurchaseReturn(supply, balance1, weight1, amount)
+        return FormulaSolidityPort.calculateSaleReturn(supply + amount, balance2, weight2, amount)
+    except:
+        return -1
+
+
+def doubleHopTestFloat(supply, balance1, weight1, balance2, weight2, amount):
+    try:
+        amount = FormulaNativePython.calculatePurchaseReturn(supply, balance1, weight1, amount)
+        return FormulaNativePython.calculateSaleReturn(supply + amount, balance2, weight2, amount)
+    except:
+        return -1
 
 
 size = int(sys.argv[1]) if len(sys.argv) > 1 else 0
@@ -36,13 +47,14 @@ for n in range(size):
     balance2 = random.randrange(1, 10 ** 23)
     weight2 = random.randrange(1, 1000000)
     amount = random.randrange(1, supply)
-    singleHop = singleHopTest(balance1, weight1, balance2, weight2, amount)
-    doubleHop = doubleHopTest(supply, balance1, weight1, balance2, weight2, amount)
-    if 0 <= singleHop <= doubleHop:
-        ratio = Decimal(singleHop) / Decimal(doubleHop)
+    singleHopFixed = singleHopTestFixed(balance1, weight1, balance2, weight2, amount)
+    doubleHopFixed = doubleHopTestFixed(supply, balance1, weight1, balance2, weight2, amount)
+    doubleHopFloat = doubleHopTestFloat(supply, balance1, weight1, balance2, weight2, amount)
+    if 0 <= doubleHopFixed <= singleHopFixed <= doubleHopFloat:
+        ratio = Decimal(singleHopFixed) / Decimal(doubleHopFloat)
         minRatio = min(minRatio, ratio)
         print('Test #{}: ratio = {:.24f}, minRatio = {:.24f}'.format(n, ratio, minRatio))
-    elif singleHop < 0:
+    elif singleHopFixed < 0 and doubleHopFixed < 0:
         ratio = Decimal(0)
         print('Test #{}: ratio = {:.24f}, minRatio = {:.24f}'.format(n, ratio, minRatio))
     else:
