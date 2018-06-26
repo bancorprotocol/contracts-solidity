7f19ec9aa61ac462ef07c815961179687c69e4ff
Or <ord@bancor.network>
2018-06-26 16:25:15 +0300

    Add connectors to the interface

--- solidity/contracts/converter/interfaces/IBancorConverter.sol
+++ solidity/contracts/converter/interfaces/IBancorConverter.sol
@@ -9,8 +9,9 @@ contract IBancorConverter {
     function getReturn(IERC20Token _fromToken, IERC20Token _toToken, uint256 _amount) public view returns (uint256);
     function convert(IERC20Token _fromToken, IERC20Token _toToken, uint256 _amount, uint256 _minReturn) public returns (uint256);
     function conversionWhitelist() public view returns (IWhitelist) {}
+    function conversionFee() public view returns (uint32) {}
+    function connectors(address _address) public view returns (uint256, uint32, bool, bool, bool) {}
+    function getConnectorBalance(IERC20Token _connectorToken) public view returns (uint256);
     // deprecated, backward compatibility
     function change(IERC20Token _fromToken, IERC20Token _toToken, uint256 _amount, uint256 _minReturn) public returns (uint256);
-    function getConnectorBalance(IERC20Token _connectorToken) public view returns (uint256);
-    function getFinalAmount(uint256 _amount, uint8 _magnitude) public view returns (uint256);
 }
