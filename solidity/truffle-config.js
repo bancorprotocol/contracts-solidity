8f93764f07964500052d85e64fa3ecf80ed8d6d7
rudo <rudoneko@gmail.com>
2018-09-10 13:34:59 +0900

    deploy/migrate時以外はVMを利用

--- solidity/truffle-config.js
+++ solidity/truffle-config.js
@@ -4,39 +4,41 @@ var WalletSubprovider = require('web3-provider-engine/subproviders/wallet.js');
 var RpcSubprovider = require('web3-provider-engine/subproviders/rpc.js')
 var Web3 = require("web3");
 
-// Insert raw hex private key here, e.g. using MyEtherWallet
-var wallet = ethwallet.fromPrivateKey(Buffer.from(process.env.ETH_DEPLOYER_KEY, 'hex'));
-var address = "0x" + wallet.getAddress().toString("hex");
-var engine = new ProviderEngine();
-engine.addProvider(new WalletSubprovider(wallet, {}));
-engine.addProvider(new RpcSubprovider({rpcUrl: "https://vicious-spider-50320.getho.io/jsonrpc"}));
-engine.start(); // Required by the provider engine.
+if ( process.argv[2] == 'migrate' ) {
+  // Insert raw hex private key here, e.g. using MyEtherWallet
+  var wallet = ethwallet.fromPrivateKey(Buffer.from(process.env.ETH_DEPLOYER_KEY, 'hex'));
+  var address = "0x" + wallet.getAddress().toString("hex");
+  var engine = new ProviderEngine();
+  engine.addProvider(new WalletSubprovider(wallet, {}));
+  engine.addProvider(new RpcSubprovider({rpcUrl: "https://vicious-spider-50320.getho.io/jsonrpc"}));
+  engine.start(); // Required by the provider engine.
 
-module.exports = {
-  networks: {
-    development: {
-      network_id: 1010,
-      gas: 4712388,
-      provider: engine,
-      from: address
+  module.exports = {
+    networks: {
+      development: {
+        network_id: 1010,
+        gas: 4712388,
+        provider: engine,
+        from: address
+      },
+      staging: {
+        network_id: 1010,
+        gas: 4712388,
+        provider: engine,
+        from: address
+      },
+      beta: {
+        network_id: 1010,
+        gas: 4712388,
+        provider: engine,
+        from: address
+      }
     },
-    staging: {
-      network_id: 1010,
-      gas: 4712388,
-      provider: engine,
-      from: address
-    },
-    beta: {
-      network_id: 1010,
-      gas: 4712388,
-      provider: engine,
-      from: address
+    solc: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
     }
-  },
-  solc: {
-    optimizer: {
-      enabled: true,
-      runs: 200
-    }
-  }
-};
+  };
+}
