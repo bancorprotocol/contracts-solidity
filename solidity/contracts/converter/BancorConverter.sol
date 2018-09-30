782f0cdbe6983ff08e938bbfb841d45c4f2b7d0b
董淑伟 <dsw0602@foxmail.com>
2018-09-30 18:29:53 +0800

    add getSaleRequire function

--- solidity/contracts/converter/BancorConverter.sol
+++ solidity/contracts/converter/BancorConverter.sol
@@ -454,6 +454,32 @@ contract BancorConverter is IBancorConverter, SmartTokenController, Managed, Con
         return getFinalAmount(amount, 1);
     }
 
+
+    /**
+       @dev returns the expected return for selling the token for one of its connector tokens
+
+       @param _connectorToken  connector token contract address
+       @param _sellAmount      amount to sell (in the smart token)
+
+       @return expected sale return amount
+   */
+    function getSaleRequire(IERC20Token _connectorToken, uint256 _sellAmount)
+    public
+    view
+    active
+    validConnector(_connectorToken)
+    returns (uint256)
+    {
+        Connector storage connector = connectors[_connectorToken];
+        uint256 tokenSupply = token.totalSupply();
+        uint256 connectorBalance = getConnectorBalance(_connectorToken);
+        IBancorFormula formula = IBancorFormula(registry.addressOf(ContractIds.BANCOR_FORMULA));
+        uint256 amount = formula.calculateSaleRequire(tokenSupply, connectorBalance, connector.weight, _sellAmount);
+
+        // return the amount minus the conversion fee
+        return getFinalAmount(amount, 1);
+    }
+
     /**
         @dev returns the expected return for selling one of the connector tokens for another connector token
 
--- solidity/contracts/converter/interfaces/IBancorFormula.sol
+++ solidity/contracts/converter/interfaces/IBancorFormula.sol
@@ -5,8 +5,9 @@ pragma solidity ^0.4.23;
 */
 contract IBancorFormula {
     function calculatePurchaseReturn(uint256 _supply, uint256 _connectorBalance, uint32 _connectorWeight, uint256 _depositAmount) public view returns (uint256);
-    function calculatePurchaseRequire(uint256 _connectorBalance, uint256 _supply, uint32 _connectorWeight, uint256 _sellAmount) public view returns (uint256);
+    function calculatePurchaseRequire(uint256 _connectorBalance, uint256 _supply, uint32 _connectorWeight, uint256 _buyAmount) public view returns (uint256);
     function calculateSaleReturn(uint256 _supply, uint256 _connectorBalance, uint32 _connectorWeight, uint256 _sellAmount) public view returns (uint256);
+    function calculateSaleRequire(uint256 _connectorBalance, uint256 _supply, uint32 _connectorWeight, uint256 _sellAmount) public view returns (uint256);
     function calculateCrossConnectorReturn(uint256 _fromConnectorBalance, uint32 _fromConnectorWeight, uint256 _toConnectorBalance, uint32 _toConnectorWeight, uint256 _amount) public view returns (uint256);
 
 }
