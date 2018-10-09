acde78df8d414e1a7d8b09b3e861109ec2a3c1ff
HackFisher <hackfisher@gmail.com>
2018-10-09 11:15:24 +0800

    add DeployAndTest.sol to include migrate and test artifacts

---
+++ solidity/contracts/DeployAndTest.sol
@@ -0,0 +1,7 @@
+pragma solidity ^0.4.23;
+
+import "../../node_modules/@evolutionland/common/contracts/StandardERC223.sol";
+import "../../node_modules/@evolutionland/common/contracts//SettingsRegistry.sol";
+
+contract DeployAndTest {
+}
