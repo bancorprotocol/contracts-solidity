67c14531ff463b210d3c5f21a80f5bb7c630462e
董淑伟 <dsw0602@foxmail.com>
2018-10-09 13:25:57 +0800

    modify migration script

--- solidity/migrations/2_deploy_migration.js
+++ solidity/migrations/2_deploy_migration.js
@@ -4,12 +4,13 @@ const BancorFormula = artifacts.require('BancorFormula');
 const BancorGasPriceLimit = artifacts.require('BancorGasPriceLimit');
 const EtherToken = artifacts.require('EtherToken');
 const ContractFeatures = artifacts.require('ContractFeatures');
-const ContractRegistry = artifacts.require('SettingsRegistry');
+const SettingsRegistry = artifacts.require('SettingsRegistry');
 const WhiteList = artifacts.require('Whitelist');
 const BancorNetwork = artifacts.require('BancorNetwork');
 const BancorExchange = artifacts.require('BancorExchange');
 const ContractIds = artifacts.require('ContractIds');
 const FeatureIds = artifacts.require('FeatureIds');
+const DeployAndTest = artifacts.require('DeployAndTest');
 
 COIN = 10**18;
 
@@ -35,8 +36,8 @@ module.exports = function(deployer) {
     //     WhiteList,
     //     EtherToken,
     // ]).then(...)
-
-    deployer.deploy(ContractRegistry);
+    deployer.deploy(DeployAndTest);
+    deployer.deploy(SettingsRegistry);
     deployer.deploy(ContractIds);
     deployer.deploy(ContractFeatures);
     deployer.deploy(BancorFormula);
@@ -44,17 +45,25 @@ module.exports = function(deployer) {
     deployer.deploy(BancorGasPriceLimit, CONF.gasPrice);
     deployer.deploy(EtherToken);
     deployer.deploy(RING, 'RING').then( async() => {
-        await deployer.deploy(BancorNetwork, ContractRegistry.address);
-        await deployer.deploy(BancorConverter, RING.address, ContractRegistry.address, 0, EtherToken.address, CONF.weight10Percent);
+        await deployer.deploy(BancorNetwork, SettingsRegistry.address);
+
+        let contractIds = await ContractIds.deployed();
+        let contractFeatures = await ContractFeatures.deployed();
+        let settingsRegistry = await SettingsRegistry.deployed();
+        let contractFeaturesId = await contractIds.CONTRACT_FEATURES.call();
+        await settingsRegistry.setAddressProperty(contractFeaturesId, contractFeatures.address);
+
+        await deployer.deploy(BancorConverter, RING.address, settingsRegistry.address, 0, EtherToken.address, CONF.weight10Percent, {gas: 8000000});
+        console.log("LOGGING: address of bancorConverter: ", BancorConverter.address);
         await deployer.deploy(BancorExchange, RING.address, BancorNetwork.address, BancorConverter.address);
 
-        let contractFeaturesId;
+
         let gasPriceLimitId;
         let formulaId;
         let bancorNetworkId;
-        let contractRegistry = await ContractRegistry.deployed();
-        let contractIds = await ContractIds.deployed();
-        let contractFeatures = await ContractFeatures.deployed();
+
+
+
         let bancorFormula = await BancorFormula.deployed();
 
         let whiteList = await WhiteList.deployed();
@@ -65,14 +74,13 @@ module.exports = function(deployer) {
         let bancorConverter = await BancorConverter.deployed();
         let ring = await RING.deployed();
 
-        contractFeaturesId = await contractIds.CONTRACT_FEATURES.call();
-        await contractRegistry.setAddressProperty(contractFeaturesId, contractFeatures.address);
+
         formulaId = await contractIds.BANCOR_FORMULA.call();
-        await contractRegistry.setAddressProperty(formulaId, bancorFormula.address);
+        await settingsRegistry.setAddressProperty(formulaId, bancorFormula.address);
         gasPriceLimitId = await contractIds.BANCOR_GAS_PRICE_LIMIT.call();
-        await contractRegistry.setAddressProperty(gasPriceLimitId, bancorGasPriceLimit.address);
+        await settingsRegistry.setAddressProperty(gasPriceLimitId, bancorGasPriceLimit.address);
         bancorNetworkId = await contractIds.BANCOR_NETWORK.call();
-        await contractRegistry.setAddressProperty(bancorNetworkId, BancorNetwork.address);
+        await settingsRegistry.setAddressProperty(bancorNetworkId, BancorNetwork.address);
 
          //do this to make SmartToken.totalSupply > 0
         await ring.issue(CONF.from, 1000000 * COIN);
