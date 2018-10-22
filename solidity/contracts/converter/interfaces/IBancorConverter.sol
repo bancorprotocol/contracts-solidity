b196d75c6d2b7f62708e88628e6c78205377a08c
yudilevi <yudilevi@gmail.com>
2018-10-22 16:01:16 +0300

    added claimTokens to IBancorConverter interface, updated builds

--- solidity/contracts/converter/interfaces/IBancorConverter.sol
+++ solidity/contracts/converter/interfaces/IBancorConverter.sol
@@ -10,8 +10,9 @@ contract IBancorConverter {
     function convert(IERC20Token _fromToken, IERC20Token _toToken, uint256 _amount, uint256 _minReturn) public returns (uint256);
     function conversionWhitelist() public view returns (IWhitelist) {}
     function conversionFee() public view returns (uint32) {}
-    function connectors(address _address) public view returns (uint256, uint32, bool, bool, bool) {}
+    function connectors(address _address) public view returns (uint256, uint32, bool, bool, bool) { _address; }
     function getConnectorBalance(IERC20Token _connectorToken) public view returns (uint256);
+    function claimTokens(address _from, uint256 _amount) public;
     // deprecated, backward compatibility
     function change(IERC20Token _fromToken, IERC20Token _toToken, uint256 _amount, uint256 _minReturn) public returns (uint256);
 }
