32184ccffe878a21bf02f0ffcb02b0b69a802628
barakman <barakman@yahoo.com>
2019-03-05 15:14:05 +0200

    Improve Python Web3Wrapper

--- solidity/python/Web3Wrapper/__init__.py
+++ solidity/python/Web3Wrapper/__init__.py
@@ -4,36 +4,43 @@ from os.path import dirname
 from json import loads
 
 
-eth = Web3(HTTPProvider("http://127.0.0.1:8545")).eth
+web3 = Web3(HTTPProvider('http://127.0.0.1:8545',request_kwargs={'timeout':60}))
+
+
+transaction = {
+    'from'    :web3.eth.accounts[0],
+    'gasPrice':web3.eth.gasPrice,
+    'gas'     :web3.eth.getBlock('latest').gasLimit
+}
 
 
 class Contract():
-    def __init__(self,moduleName,ownerAddress='',args=[]):
+    def __init__(self,moduleName,args=[]):
         path = dirname(dirname(dirname(__file__)))+'/build/'
         abi = open(path+moduleName+'.abi').read()
         bin = open(path+moduleName+'.bin').read()
-        self.contract = eth.contract(abi=loads(abi),bytecode=bin)
-        self.transact = {'from':ownerAddress if ownerAddress else eth.accounts[0]}
-        self.address  = eth.getTransactionReceipt(self.contract.deploy(transaction=self.transact,args=args))['contractAddress']
-    def owner(self):
-        return self.transact['from']
-    def getter(self):
-        return self.contract(self.address).call(self.transact)
-    def setter(self):
-        return self.contract(self.address).transact(self.transact)
-    def tester(self):
-        return self.contract(self.address).estimateGas(self.transact)
+        self.contract = web3.eth.contract(abi=loads(abi),bytecode=bin)
+        self.address  = web3.eth.getTransactionReceipt(self.contract.deploy(transaction,args))['contractAddress']
+    def balance(self):
+        return web3.eth.getBalance(self.address)
+    def getter(self,extension={}):
+        return self.contract(self.address).call({**transaction,**extension})
+    def setter(self,extension={}):
+        return self.contract(self.address).transact({**transaction,**extension})
+    def tester(self,extension={}):
+        return self.contract(self.address).estimateGas({**transaction,**extension})
     def decode(hash,index,params):
         event = {}
         index1 = 1
         index2 = 2
-        log = eth.getTransactionReceipt(hash)['logs'][index]
+        log = web3.eth.getTransactionReceipt(hash)['logs'][index]
         for param in params:
             if param['indexed']:
-                event[param['name']] = int(log['topics'][index1],16)
+                event[param['name']] = int(log['topics'][index1].hex(),16)
                 index1 += 1
             else:
-                size = param['size']//4
-                event[param['name']] = int(log['data'][index2:index2+size],16)
-                index2 += size
+                event[param['name']] = int(log['data'][index2:index2+param['size']//4],16)
+                index2 += param['size']//4
         return event
+    def jump(seconds):
+        web3.providers[0].make_request('evm_increaseTime',[seconds])
