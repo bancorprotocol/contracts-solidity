253acd1cfdfd5a8604c7ac46fa552eb63c1a2081
barakman <barakman@yahoo.com>
2019-07-03 17:38:27 +0300

    Minor.

--- scripts/generate-docs.js
+++ scripts/generate-docs.js
@@ -1,7 +1,7 @@
 const NODE_DIR = "node_modules";
 const CONS_DIR = "solidity/contracts";
 const DOCS_DIR = "docs";
-const SKIP_DIR = /helpers/;
+const SKIP_DIR = ["helpers"];
 
 const fs        = require("fs");
 const spawnSync = require("child_process").spawnSync;
@@ -10,7 +10,7 @@ fs.writeFileSync(CONS_DIR + "/README.md", "");
 fs.writeFileSync("SUMMARY.md", `* [main](${DOCS_DIR}/index.md)\n`);
 
 for (const fileName of fs.readdirSync(CONS_DIR)) {
-    if (fs.lstatSync(CONS_DIR + "/" + fileName).isDirectory() && !SKIP_DIR.test(fileName)) {
+    if (fs.lstatSync(CONS_DIR + "/" + fileName).isDirectory() && !SKIP_DIR.includes(fileName)) {
         fs.writeFileSync(CONS_DIR + "/" + fileName + "/README.md", "");
         fs.appendFileSync("SUMMARY.md", `* [${fileName}](${DOCS_DIR}/${fileName}.md)\n`);
     }
@@ -20,7 +20,7 @@ spawnSync("node", [
     NODE_DIR + "/solidity-docgen/dist/cli.js",
     "--contractsDir=" + CONS_DIR,
     "--outputDir="    + DOCS_DIR,
-    "--ignore="       + SKIP_DIR,
+    "--ignore="       + SKIP_DIR.join(","),
     "--templateFile=" + CONS_DIR + "/../docgen.hbs",
     "--solcModule="   + NODE_DIR + "/truffle/node_modules/solc"
 ]);
