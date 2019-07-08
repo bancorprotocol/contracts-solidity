f889a75e57ac1cb203dc360c484ae884f38b06e3
barakman <barakman@yahoo.com>
2019-07-08 13:21:56 +0300

    Improve auto-documentation.

--- scripts/generate-docs.js
+++ scripts/generate-docs.js
@@ -1,20 +1,42 @@
 const NODE_DIR = "node_modules";
 const CONS_DIR = "solidity/contracts";
 const DOCS_DIR = "docs";
-const SKIP_DIR = ["helpers"];
+
+// Skip any file or folder whose name is in the list below
+const SKIP_LIST = [
+    "bancorx/interfaces",
+    "converter/interfaces",
+    "crowdsale",
+    "helpers",
+    "legacy",
+    "token/interfaces",
+    "utility/interfaces",
+    "ContractIds.sol",
+    "FeatureIds.sol",
+    "IBancorNetwork.sol"
+];
 
 const fs        = require("fs");
+const basename  = require("path").basename;
 const spawnSync = require("child_process").spawnSync;
 
 fs.writeFileSync(CONS_DIR + "/README.md", "");
-fs.writeFileSync("SUMMARY.md", `* [main](${DOCS_DIR}/index.md)\n`);
+fs.writeFileSync("SUMMARY.md", "# Summary\n");
 
-for (const fileName of fs.readdirSync(CONS_DIR)) {
-    if (fs.lstatSync(CONS_DIR + "/" + fileName).isDirectory() && !SKIP_DIR.includes(fileName)) {
-        fs.writeFileSync(CONS_DIR + "/" + fileName + "/README.md", "");
-        fs.appendFileSync("SUMMARY.md", `* [${fileName}](${DOCS_DIR}/${fileName}.md)\n`);
+function scanDir(pathName = CONS_DIR, indentation = "") {
+    if (!SKIP_LIST.map(x => CONS_DIR + "/" + x).includes(pathName)) {
+        if (fs.lstatSync(pathName).isDirectory()) {
+            fs.appendFileSync("SUMMARY.md", indentation + "* " + basename(pathName) + ":\n");
+            for (const fileName of fs.readdirSync(pathName))
+                scanDir(pathName + "/" + fileName, indentation + "  ");
+        }
+        else if (pathName.endsWith(".sol")) {
+            fs.appendFileSync("SUMMARY.md", indentation + "* [" + basename(pathName).replace(".sol", "") + "](" + DOCS_DIR + "/" + basename(pathName).replace(".sol", ".md") + ")\n");
+        }
     }
 }
+
+scanDir();
 
 function runNode(args) {
     const result = spawnSync("node", args)
@@ -27,10 +49,12 @@ runNode([
     NODE_DIR + "/solidity-docgen/dist/cli.js",
     "--contractsDir=" + CONS_DIR,
     "--outputDir="    + DOCS_DIR,
-    "--ignore="       + SKIP_DIR.join(","),
     "--templateFile=" + CONS_DIR + "/../docgen.hbs",
     "--solcModule="   + NODE_DIR + "/truffle/node_modules/solc"
 ]);
+
+for (const contractDoc of fs.readFileSync(DOCS_DIR + "/index.md").toString().split("# Contract "))
+    fs.writeFileSync(DOCS_DIR + "/" + contractDoc.split("`")[1] + ".md", "# Contract " + contractDoc);
 
 runNode([
     NODE_DIR + "/gitbook-cli/bin/gitbook.js",
