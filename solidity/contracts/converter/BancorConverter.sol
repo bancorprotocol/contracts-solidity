53be861ea49459a992b0ba84921871484f338322
yudilevi <yudilevi@gmail.com>
2019-07-10 14:42:22 +0300

    updated docs

--- solidity/contracts/converter/BancorConverter.sol
+++ solidity/contracts/converter/BancorConverter.sol
@@ -984,7 +984,9 @@ contract BancorConverter is IBancorConverter, SmartTokenController, Managed, Con
         }
     }
 
-    // deprecated, backward compatibility
+    /**
+        @dev deprecated, backward compatibility
+    */
     function change(IERC20Token _fromToken, IERC20Token _toToken, uint256 _amount, uint256 _minReturn) public returns (uint256) {
         return convertInternal(_fromToken, _toToken, _amount, _minReturn);
     }
