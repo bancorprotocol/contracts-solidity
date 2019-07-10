8cd30df353ccc32deb507db64efd9adc26be4a5f
barakman <barakman@yahoo.com>
2019-07-10 14:47:54 +0300

    Update testing & coverage infrastructure.

--- solidity/truffle-config.js
+++ solidity/truffle-config.js
@@ -4,39 +4,35 @@ module.exports = {
         development: {
             host:       "localhost",
             port:       7545,
-            network_id: "*",          // Match any network id
-            gasPrice:   20000000000,  // Gas price used for deploys
-            gas:        5712388       // Gas limit used for deploys
+            network_id: "*",         // Match any network id
+            gasPrice:   20000000000, // Gas price used for deploys
+            gas:        5712388      // Gas limit used for deploys
         },
         production: {
             host:       "localhost",
             port:       7545,
-            network_id: "*",          // Match any network id
-            gasPrice:   20000000000,  // Gas price used for deploys
-            gas:        5712388       // Gas limit used for deploys
+            network_id: "*",         // Match any network id
+            gasPrice:   20000000000, // Gas price used for deploys
+            gas:        5712388      // Gas limit used for deploys
         },
         coverage: {     // See <https://www.npmjs.com/package/solidity-coverage#network-configuration>
             host:       "localhost",
-            port:       7555,         // Also in .solcover.js
-            network_id: "*",          // Match any network id
-            gasPrice:   0x01,         // Gas price used for deploys
-            gas:        0xfffffffffff // Gas limit used for deploys
+            port:       7555,            // Also in .solcover.js
+            network_id: "*",             // Match any network id
+            gasPrice:   0x1,             // Gas price used for deploys
+            gas:        0x1fffffffffffff // Gas limit used for deploys
         }
     },
     mocha: {
         enableTimeouts: false,
         useColors:      true,
-        bail:           true
+        bail:           true,
+        reporter:       "list" // See <https://mochajs.org/#reporters>
     },
     solc: {
         optimizer: {
             enabled: true,
             runs:    200
         }
-    },
-    compilers: {
-        solc: {
-            version: "0.4.24"
-        }
-    }  
+    }
 };
