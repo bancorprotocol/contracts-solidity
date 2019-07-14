7bb008e0bee868c0789244ecc615cecc1fc0a9ea
cgewecke <christophergewecke@gmail.com>
2019-07-14 14:10:28 -0700

    Upgrade solidity-coverage to 0.6.2

--- scripts/fix-modules.js
+++ scripts/fix-modules.js
@@ -14,7 +14,7 @@ function fix(fileName, tokens) {
         let data = fs.readFileSync(fileName, {encoding: "utf8"});
         for (const token of tokens)
             data = data.split(token.prev).join(token.next);
-        fs.writeFileSync(fileName, data, {encoding: "utf8"});    
+        fs.writeFileSync(fileName, data, {encoding: "utf8"});
     }
     catch (error) {
         console.log(error.message);
@@ -25,11 +25,6 @@ fix("./node_modules/truffle/build/cli.bundled.js", [
     {prev: "request = new XHR2", next: "request = new XMLHttpRequest"},
     {prev: "error = errors.InvalidResponse", next: "error = payload.method === 'evm_revert' || payload.method === 'evm_snapshot' ? null : errors.InvalidResponse"},
     {prev: "display_path = \".\" + path.sep + path.relative(options.working_directory, import_path);", next: "if (options.fix_paths) {display_path = \".\" + path.sep + path.relative(options.working_directory, import_path); result[display_path] = result[import_path]; delete result[import_path];}"}]
-);
-
-fix("./node_modules/solidity-coverage/lib/app.js", [
-    {prev: "events.push", next: "coverage.processEvent"},
-    {prev: "readFileSync(contractPath).toString()", next: "readFileSync(contractPath).toString().replace('/* SC0 */', '// SC0 //').replace('// SC1 //', '/* SC1 */')"}]
 );
 
 fix("./node_modules/solidity-coverage/lib/coverageMap.js", [
