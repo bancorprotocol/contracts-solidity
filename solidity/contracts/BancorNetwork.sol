e9894e79d41450885223830de2431e6a99b54bfd
Yudi Levi <yudilevi@users.noreply.github.com>
2019-08-15 14:34:15 +0300

    updated docs

--- solidity/contracts/BancorNetwork.sol
+++ solidity/contracts/BancorNetwork.sol
@@ -21,7 +21,7 @@ import './bancorx/interfaces/IBancorX.sol';
 
     A note on Conversion Path: Conversion path is a data structure that is used when converting a token to another token in the Bancor Network when the conversion cannot necessarily be done by a single converter and might require multiple 'hops'. The path defines which converters should be used and what kind of conversion should be done in each step. 
     
-    The path format doesn't include complex structure; instead, it is represented by a single array in which each 'hop' is represented by a 2-tuple - smart token & to token. In addition, the first element is always the source token. The smart token is only used as a pointer to a converter (since converter addresses are more likely to change).
+    The path format doesn't include complex structure; instead, it is represented by a single array in which each 'hop' is represented by a 2-tuple - smart token & to token. In addition, the first element is always the source token. The smart token is only used as a pointer to a converter (since converter addresses are more likely to change as opposed to smart token addresses).
 
     Format:
     [source token, smart token, to token, smart token, to token...]
