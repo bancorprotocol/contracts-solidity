a1637e7b8c125ef058fe21e2c7e05edaa6110964
barakman <barakman@yahoo.com>
2019-08-25 16:21:11 +0300

    Add functions: - `claimAndConvert2` - `claimAndConvertFor2` - `convert2` - `convertFor2`

--- solidity/contracts/IBancorNetwork.sol
+++ solidity/contracts/IBancorNetwork.sol
@@ -5,8 +5,10 @@ import './token/interfaces/IERC20Token.sol';
     Bancor Network interface
 */
 contract IBancorNetwork {
-    function convert(IERC20Token[] _path, uint256 _amount, uint256 _minReturn) public payable returns (uint256);
-    function convertFor(IERC20Token[] _path, uint256 _amount, uint256 _minReturn, address _for) public payable returns (uint256);
+    function convert2(IERC20Token[] _path, uint256 _amount, uint256 _minReturn, address _affiliateAccount, uint256 _affiliateFee) public payable returns (uint256);
+    function convertFor2(IERC20Token[] _path, uint256 _amount, uint256 _minReturn, address _for, address _affiliateAccount, uint256 _affiliateFee) public payable returns (uint256);
+    function claimAndConvert2(IERC20Token[] _path, uint256 _amount, uint256 _minReturn, address _affiliateAccount, uint256 _affiliateFee) public returns (uint256);
+    function claimAndConvertFor2(IERC20Token[] _path, uint256 _amount, uint256 _minReturn, address _for, address _affiliateAccount, uint256 _affiliateFee) public returns (uint256);
 
     function convertForPrioritized4(
         IERC20Token[] _path,
@@ -21,6 +23,18 @@ contract IBancorNetwork {
         address _affiliateAccount,
         uint256 _affiliateFee
     ) public payable returns (uint256);
+
+    // deprecated, backward compatibility
+    function convert(IERC20Token[] _path, uint256 _amount, uint256 _minReturn) public payable returns (uint256);
+
+    // deprecated, backward compatibility
+    function convertFor(IERC20Token[] _path, uint256 _amount, uint256 _minReturn, address _for) public payable returns (uint256);
+
+    // deprecated, backward compatibility
+    function claimAndConvert2(IERC20Token[] _path, uint256 _amount, uint256 _minReturn) public returns (uint256);
+
+    // deprecated, backward compatibility
+    function claimAndConvertFor2(IERC20Token[] _path, uint256 _amount, uint256 _minReturn, address _for) public returns (uint256);
 
     // deprecated, backward compatibility
     function convertForPrioritized3(
