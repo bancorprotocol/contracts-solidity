1a528f663d23239443cc3253bc1dc22f764cad58
barakman <barakman@yahoo.com>
2019-08-27 10:21:50 +0300

    Add tests for function `BancorNetwork.quickConvertPrioritized2`.

--- solidity/test/BancorNetwork.js
+++ solidity/test/BancorNetwork.js
@@ -634,4 +634,91 @@ contract('BancorNetwork', accounts => {
 
         await utils.catchRevert(converter1.quickConvertPrioritized(smartToken1BuyPath, 100, 1, maximumBlock, result.v, result.r, result.s, { from: accounts[1], value: 100 }));
     });
+
+    it('verifies prioritized quick buy with trusted signature', async () => {
+        let prevBalance = await smartToken1.balanceOf.call(accounts[1]);
+
+        let maximumBlock = web3.eth.blockNumber + 100;
+        let gasPrice = BancorGasPriceLimit.class_defaults.gasPrice;
+
+        let soliditySha3 = web3Utils.soliditySha3(maximumBlock, gasPrice, accounts[1], converter1.address, 100, {'type': 'address', 'value': smartToken1BuyPath});
+        let result = sign(soliditySha3, trustedAddress);
+
+        await converter1.quickConvertPrioritized2(smartToken1BuyPath, 100, 1, [100, maximumBlock, result.v, result.r, result.s], utils.zeroAddress, 0, { from: accounts[1], value: 100 });
+        let newBalance = await smartToken1.balanceOf.call(accounts[1]);
+        assert.isAbove(newBalance.toNumber(), prevBalance.toNumber(), "new balance isn't higher than previous balance");
+    });
+
+    it('should throw when calling quickConvertPrioritized2 with untrusted signature', async () => {
+        let maximumBlock = web3.eth.blockNumber + 100;
+        let gasPrice = BancorGasPriceLimit.class_defaults.gasPrice;
+
+        let soliditySha3 = web3Utils.soliditySha3(maximumBlock, gasPrice, accounts[1], converter1.address, 100, {'type': 'address', 'value': smartToken1BuyPath});
+        let result = sign(soliditySha3, untrustedAddress);
+
+        await utils.catchRevert(converter1.quickConvertPrioritized2(smartToken1BuyPath, 100, 1, [100, maximumBlock, result.v, result.r, result.s], utils.zeroAddress, 0, { from: accounts[1], value: 100 }));
+    });
+
+    it('should throw when calling quickConvertPrioritized2 with wrong path', async () => {
+        let wrongPath = [etherToken.address, smartToken1.address, smartToken1.address, smartToken1.address, smartToken1.address];
+        let maximumBlock = web3.eth.blockNumber + 100;
+        let gasPrice = BancorGasPriceLimit.class_defaults.gasPrice;
+
+        let soliditySha3 = web3Utils.soliditySha3(maximumBlock, gasPrice, accounts[1], converter1.address, 100, {'type': 'address', 'value': wrongPath});
+        let result = sign(soliditySha3, trustedAddress);
+
+        await utils.catchRevert(converter1.quickConvertPrioritized2(smartToken1BuyPath, 100, 1, [100, maximumBlock, result.v, result.r, result.s], utils.zeroAddress, 0, { from: accounts[1], value: 100 }));
+    });
+
+    it('should throw when calling quickConvertPrioritized2 with wrong amount', async () => {
+        let maximumBlock = web3.eth.blockNumber + 100;
+        let gasPrice = BancorGasPriceLimit.class_defaults.gasPrice;
+
+        let soliditySha3 = web3Utils.soliditySha3(maximumBlock, gasPrice, accounts[1], converter1.address, 100, {'type': 'address', 'value': smartToken1BuyPath});
+        let result = sign(soliditySha3, trustedAddress);
+
+        await utils.catchRevert(converter1.quickConvertPrioritized2(smartToken1BuyPath, 200, 1, [200, maximumBlock, result.v, result.r, result.s], utils.zeroAddress, 0, { from: accounts[1], value: 100 }));
+    });
+
+    it('should throw when calling quickConvertPrioritized2 with higher block number than what appears in the signing data', async () => {
+        let maximumBlock = web3.eth.blockNumber + 100;
+        let wrongBlockNumber = maximumBlock + 100;
+        let gasPrice = BancorGasPriceLimit.class_defaults.gasPrice;
+
+        let soliditySha3 = web3Utils.soliditySha3(maximumBlock, gasPrice, accounts[1], converter1.address, 100, {'type': 'address', 'value': smartToken1BuyPath});
+        let result = sign(soliditySha3, trustedAddress);
+
+        await utils.catchRevert(converter1.quickConvertPrioritized2(smartToken1BuyPath, 100, 1, [100, wrongBlockNumber, result.v, result.r, result.s], utils.zeroAddress, 0, { from: accounts[1], value: 100 }));
+    });
+
+    it('should throw when calling quickConvertPrioritized2 with lower block number than what appears in the signing data', async () => {
+        let maximumBlock = web3.eth.blockNumber + 100;
+        let wrongBlockNumber = maximumBlock - 1;
+        let gasPrice = BancorGasPriceLimit.class_defaults.gasPrice;
+
+        let soliditySha3 = web3Utils.soliditySha3(maximumBlock, gasPrice, accounts[1], converter1.address, 100, {'type': 'address', 'value': smartToken1BuyPath});
+        let result = sign(soliditySha3, trustedAddress);
+
+        await utils.catchRevert(converter1.quickConvertPrioritized2(smartToken1BuyPath, 100, 1, [100, wrongBlockNumber, result.v, result.r, result.s], utils.zeroAddress, 0, { from: accounts[1], value: 100 }));
+    });
+
+    it('should throw when calling quickConvertPrioritized2 with higher gas price than what appears in the signing data', async () => {
+        let maximumBlock = web3.eth.blockNumber + 100;
+        let gasPrice = BancorGasPriceLimit.class_defaults.gasPrice - 1;
+
+        let soliditySha3 = web3Utils.soliditySha3(maximumBlock, gasPrice, accounts[1], converter1.address, 100, {'type': 'address', 'value': smartToken1BuyPath});
+        let result = sign(soliditySha3, trustedAddress);
+
+        await utils.catchRevert(converter1.quickConvertPrioritized2(smartToken1BuyPath, 100, 1, [100, maximumBlock, result.v, result.r, result.s], utils.zeroAddress, 0, { from: accounts[1], value: 100 }));
+    });
+
+    it('should throw when calling quickConvertPrioritized2 with lower gas price than what appears in the signing data', async () => {
+        let maximumBlock = web3.eth.blockNumber + 100;
+        let gasPrice = BancorGasPriceLimit.class_defaults.gasPrice + 1;
+
+        let soliditySha3 = web3Utils.soliditySha3(maximumBlock, gasPrice, accounts[1], converter1.address, 100, {'type': 'address', 'value': smartToken1BuyPath});
+        let result = sign(soliditySha3, trustedAddress);
+
+        await utils.catchRevert(converter1.quickConvertPrioritized2(smartToken1BuyPath, 100, 1, [100, maximumBlock, result.v, result.r, result.s], utils.zeroAddress, 0, { from: accounts[1], value: 100 }));
+    });
 });
