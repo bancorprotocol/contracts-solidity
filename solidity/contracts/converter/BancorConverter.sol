553d37db34d111ba72657df07ae7d5815f51139f
barakman <barakman@yahoo.com>
2019-08-28 18:37:47 +0300

    Remove unused modifier `validConversionPath`.

--- solidity/contracts/converter/BancorConverter.sol
+++ solidity/contracts/converter/BancorConverter.sol
@@ -186,12 +186,6 @@ contract BancorConverter is IBancorConverter, SmartTokenController, Managed, Con
         _;
     }
 
-    // validates a conversion path - verifies that the number of elements is odd and that maximum number of 'hops' is 10
-    modifier validConversionPath(IERC20Token[] _path) {
-        require(_path.length > 2 && _path.length <= (1 + 2 * 10) && _path.length % 2 == 1);
-        _;
-    }
-
     // allows execution only when the total weight is 100%
     modifier maxTotalWeightOnly() {
         require(totalConnectorWeight == MAX_WEIGHT);
