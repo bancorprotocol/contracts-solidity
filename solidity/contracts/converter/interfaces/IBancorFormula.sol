7195736028c380bacbc42bdc05e890d33cbfdc09
barakman <barakman@yahoo.com>
2019-09-22 13:26:36 +0300

    Cosmetic.

--- solidity/contracts/converter/interfaces/IBancorFormula.sol
+++ solidity/contracts/converter/interfaces/IBancorFormula.sol
@@ -7,8 +7,8 @@ contract IBancorFormula {
     function calculatePurchaseReturn(uint256 _supply, uint256 _reserveBalance, uint32 _reserveRatio, uint256 _depositAmount) public view returns (uint256);
     function calculateSaleReturn(uint256 _supply, uint256 _reserveBalance, uint32 _reserveRatio, uint256 _sellAmount) public view returns (uint256);
     function calculateCrossReserveReturn(uint256 _fromReserveBalance, uint32 _fromReserveRatio, uint256 _toReserveBalance, uint32 _toReserveRatio, uint256 _amount) public view returns (uint256);
-    // deprecated, backward compatibility
-    function calculateCrossConnectorReturn(uint256 _fromConnectorBalance, uint32 _fromConnectorWeight, uint256 _toConnectorBalance, uint32 _toConnectorWeight, uint256 _amount) public view returns (uint256);
     function calculateFundReturn(uint256 _supply, uint256 _connectorBalance, uint32 _totalWeight, uint256 _amount) public view returns (uint256);
     function calculateLiquidateReturn(uint256 _supply, uint256 _connectorBalance, uint32 _totalWeight, uint256 _amount) public view returns (uint256);
+    // deprecated, backward compatibility
+    function calculateCrossConnectorReturn(uint256 _fromConnectorBalance, uint32 _fromConnectorWeight, uint256 _toConnectorBalance, uint32 _toConnectorWeight, uint256 _amount) public view returns (uint256);
 }
