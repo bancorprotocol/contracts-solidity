4ea5c46ae569c21fcc3c2da67fc4ae63f4e9088b
barakman <barakman@yahoo.com>
2019-10-06 09:47:58 +0300

    Remove dummy implementation of functions in "interfaces", where possible (in IBancorGasPriceLimit, IERC20Token and INonStandardERC20, but not in IBancorConverter and IOwned).

--- solidity/contracts/converter/interfaces/IBancorGasPriceLimit.sol
+++ solidity/contracts/converter/interfaces/IBancorGasPriceLimit.sol
@@ -4,6 +4,6 @@ pragma solidity 0.4.26;
     Bancor Gas Price Limit interface
 */
 contract IBancorGasPriceLimit {
-    function gasPrice() public view returns (uint256) {}
+    function gasPrice() public view returns (uint256);
     function validateGasPrice(uint256) public view;
 }
