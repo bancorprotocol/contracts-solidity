e1b20ed98ed881776ed0fc8a84d2b6d9b50a9168
barakman <barakman@yahoo.com>
2019-12-17 15:47:59 +0200

    Fix `verify_network_path_finder.js` according to recent change in the `BancorNetworkPathFinder` contract.

--- solidity/utils/verify_network_path_finder.js
+++ solidity/utils/verify_network_path_finder.js
@@ -2,26 +2,27 @@ const fs     = require("fs");
 const Web3   = require("web3");
 const assert = require("assert");
 
-const NODE_ADDRESS   = process.argv[2];
-const FINDER_ADDRESS = process.argv[3];
+const NODE_ADDRESS        = process.argv[2];
+const PATH_FINDER_ADDRESS = process.argv[3];
 
-const FINDER_ABI      = JSON.parse(fs.readFileSync(__dirname + "/../build/BancorNetworkPathFinder.abi"));
-const REGISTRY_ABI    = JSON.parse(fs.readFileSync(__dirname + "/../build/BancorConverterRegistry.abi"));
-const CONVERTER_ABI   = JSON.parse(fs.readFileSync(__dirname + "/../build/BancorConverter.abi"        ));
-const SMART_TOKEN_ABI = JSON.parse(fs.readFileSync(__dirname + "/../build/SmartToken.abi"             ));
+const PATH_FINDER_ABI        = JSON.parse(fs.readFileSync(__dirname + "/../build/BancorNetworkPathFinder.abi"));
+const CONTRACT_REGISTRY_ABI  = JSON.parse(fs.readFileSync(__dirname + "/../build/ContractRegistry.abi"       ));
+const CONVERTER_REGISTRY_ABI = JSON.parse(fs.readFileSync(__dirname + "/../build/BancorConverterRegistry.abi"));
+const SMART_TOKEN_ABI        = JSON.parse(fs.readFileSync(__dirname + "/../build/SmartToken.abi"             ));
+const CONVERTER_ABI          = JSON.parse(fs.readFileSync(__dirname + "/../build/BancorConverter.abi"        ));
 
-async function generatePath(web3, sourceToken, targetToken, anchorToken, registry) {
-    const sourcePath = await getPath(web3, sourceToken, anchorToken, registry);
-    const targetPath = await getPath(web3, targetToken, anchorToken, registry);
+async function generatePath(web3, sourceToken, targetToken, anchorToken, converterRegistry) {
+    const sourcePath = await getPath(web3, sourceToken, anchorToken, converterRegistry);
+    const targetPath = await getPath(web3, targetToken, anchorToken, converterRegistry);
     return getShortestPath(sourcePath, targetPath);
 }
 
-async function getPath(web3, token, anchorToken, registryContract) {
+async function getPath(web3, token, anchorToken, converterRegistry) {
     if (token == anchorToken)
         return [token];
 
-    const isSmartToken = await rpc(registryContract.methods.isSmartToken(token));
-    const smartTokens = isSmartToken ? [token] : await rpc(registryContract.methods.getConvertibleTokenSmartTokens(token));
+    const isSmartToken = await rpc(converterRegistry.methods.isSmartToken(token));
+    const smartTokens = isSmartToken ? [token] : await rpc(converterRegistry.methods.getConvertibleTokenSmartTokens(token));
     for (const smartToken of smartTokens) {
         const smartTokenContract = new web3.eth.Contract(SMART_TOKEN_ABI, smartToken);
         const converterContract = new web3.eth.Contract(CONVERTER_ABI, await rpc(smartTokenContract.methods.owner()));
@@ -29,7 +30,7 @@ async function getPath(web3, token, anchorToken, registryContract) {
         for (let i = 0; i < connectorTokenCount; i++) {
             const connectorToken = await rpc(converterContract.methods.connectorTokens(i));
             if (connectorToken != token) {
-                const path = await getPath(web3, connectorToken, anchorToken, registryContract);
+                const path = await getPath(web3, connectorToken, anchorToken, converterRegistry);
                 if (path.length > 0)
                     return [token, smartToken, ...path];
             }
@@ -113,18 +114,19 @@ function print(convertibleTokens, i, j, sourceSymbol, targetSymbol, path) {
 
 async function run() {
     const web3 = new Web3(NODE_ADDRESS);
-    const finder = new web3.eth.Contract(FINDER_ABI, FINDER_ADDRESS);
-    const registry = new web3.eth.Contract(REGISTRY_ABI, await rpc(finder.methods.converterRegistry()));
+    const pathFinder = new web3.eth.Contract(PATH_FINDER_ABI, PATH_FINDER_ADDRESS);
+    const contractRegistry = new web3.eth.Contract(CONTRACT_REGISTRY_ABI, await rpc(pathFinder.methods.registry()));
+    const converterRegistry = new web3.eth.Contract(CONVERTER_REGISTRY_ABI, await rpc(contractRegistry.methods.addressOf(Web3.utils.asciiToHex("BancorConverterRegistry"))));
 
-    const anchorToken = await rpc(finder.methods.anchorToken());
-    const convertibleTokens = await rpc(registry.methods.getConvertibleTokens());
+    const anchorToken = await rpc(pathFinder.methods.anchorToken());
+    const convertibleTokens = await rpc(converterRegistry.methods.getConvertibleTokens());
 
     for (let i = 0; i < convertibleTokens.length; i++) {
         const sourceSymbol = await symbol(web3, convertibleTokens[i]);
         for (let j = 0; j < convertibleTokens.length; j++) {
             const targetSymbol = await symbol(web3, convertibleTokens[j]);
-            const expected = await generatePath(web3, convertibleTokens[i], convertibleTokens[j], anchorToken, registry);
-            const actual = await rpc(finder.methods.generatePath(convertibleTokens[i], convertibleTokens[j]));
+            const expected = await generatePath(web3, convertibleTokens[i], convertibleTokens[j], anchorToken, converterRegistry);
+            const actual = await rpc(pathFinder.methods.generatePath(convertibleTokens[i], convertibleTokens[j]));
             const path = await Promise.all(actual.map(token => symbol(web3, token)));
             print(convertibleTokens, i, j, sourceSymbol, targetSymbol, path);
             assert.equal(`${actual}`, `${expected}`);
