fe09bfd9586bc823cac34a3089f2fa02cba8ae9f
barakman <barakman@yahoo.com>
2020-01-12 20:32:21 +0200

    Cosmetic (remove trailing tabs).

--- solidity/contracts/converter/BancorConverter.sol
+++ solidity/contracts/converter/BancorConverter.sol
@@ -875,11 +875,11 @@ contract BancorConverter is IBancorConverter, SmartTokenController, Managed, Con
         return signature;
     }
 
-    /**	
-      * @dev deprecated, backward compatibility	
-    */	
-    function change(IERC20Token _fromToken, IERC20Token _toToken, uint256 _amount, uint256 _minReturn) public returns (uint256) {	
-        return convertInternal(_fromToken, _toToken, _amount, _minReturn);	
+    /**
+      * @dev deprecated, backward compatibility
+    */
+    function change(IERC20Token _fromToken, IERC20Token _toToken, uint256 _amount, uint256 _minReturn) public returns (uint256) {
+        return convertInternal(_fromToken, _toToken, _amount, _minReturn);
     }
 
     /**
