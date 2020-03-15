3b73f4c59adc601a2a8ea753676062e9af3839e0
barakman <barakman@yahoo.com>
2020-03-15 10:48:16 +0200

    Code cleanup (remove redundant parenthesis).

--- solidity/python/FormulaNativePython/__init__.py
+++ solidity/python/FormulaNativePython/__init__.py
@@ -7,29 +7,29 @@ getcontext().prec = 80 # 78 digits for a maximum of 2^256-1, and 2 more digits f
 
 def calculatePurchaseReturn(supply, balance, ratio, amount):
     supply, balance, ratio, amount = [Decimal(value) for value in vars().values()]
-    return (supply)*((1+(amount)/(balance))**((ratio)/1000000)-1)
+    return supply*((1+amount/balance)**(ratio/1000000)-1)
 
 
 def calculateSaleReturn(supply, balance, ratio, amount):
     supply, balance, ratio, amount = [Decimal(value) for value in vars().values()]
-    return (balance)*(1-(1-(amount)/(supply))**(1000000/(ratio)))
+    return balance*(1-(1-amount/supply)**(1000000/ratio))
 
 
 def calculateCrossReserveReturn(balance1, ratio1, balance2, ratio2, amount):
     balance1, ratio1, balance2, ratio2, amount = [Decimal(value) for value in vars().values()]
-    return (balance2)*(1-((balance1)/(balance1+amount))**((ratio1)/(ratio2)))
+    return balance2*(1-(balance1/(balance1+amount))**(ratio1/ratio2))
 
 
 def calculateFundCost(supply, balance, ratios, amount):
     supply, balance, ratios, amount = [Decimal(value) for value in vars().values()]
-    return (balance)*(((supply+amount)/(supply))**(1000000/(ratios))-1)
+    return balance*(((supply+amount)/supply)**(1000000/ratios)-1)
 
 
 def calculateLiquidateReturn(supply, balance, ratios, amount):
     supply, balance, ratios, amount = [Decimal(value) for value in vars().values()]
-    return (balance)*(1-((supply-amount)/(supply))**(1000000/(ratios)))
+    return balance*(1-((supply-amount)/supply)**(1000000/ratios))
 
 
 def power(baseN, baseD, expN, expD, precision):
     baseN, baseD, expN, expD, precision = [Decimal(value) for value in vars().values()]
-    return ((baseN)/(baseD))**((expN)/(expD))*2**precision
+    return (baseN/baseD)**(expN/expD)*2**precision
