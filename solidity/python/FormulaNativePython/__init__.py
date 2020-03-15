bf7395416fc067f69babb6d8df688ea7412dda0f
barakman <barakman@yahoo.com>
2020-03-15 10:40:41 +0200

    Improve coding in file `FormulaNativePython/__init__.py`.

--- solidity/python/FormulaNativePython/__init__.py
+++ solidity/python/FormulaNativePython/__init__.py
@@ -6,24 +6,30 @@ getcontext().prec = 80 # 78 digits for a maximum of 2^256-1, and 2 more digits f
 
 
 def calculatePurchaseReturn(supply, balance, ratio, amount):
-    return Decimal(supply)*((1+Decimal(amount)/Decimal(balance))**(Decimal(ratio)/1000000)-1)
+    supply, balance, ratio, amount = [Decimal(value) for value in vars().values()]
+    return (supply)*((1+(amount)/(balance))**((ratio)/1000000)-1)
 
 
 def calculateSaleReturn(supply, balance, ratio, amount):
-    return Decimal(balance)*(1-(1-Decimal(amount)/Decimal(supply))**(1000000/Decimal(ratio)))
+    supply, balance, ratio, amount = [Decimal(value) for value in vars().values()]
+    return (balance)*(1-(1-(amount)/(supply))**(1000000/(ratio)))
 
 
 def calculateCrossReserveReturn(balance1, ratio1, balance2, ratio2, amount):
-    return Decimal(balance2)*(1-(Decimal(balance1)/Decimal(balance1+amount))**(Decimal(ratio1)/Decimal(ratio2)))
+    balance1, ratio1, balance2, ratio2, amount = [Decimal(value) for value in vars().values()]
+    return (balance2)*(1-((balance1)/(balance1+amount))**((ratio1)/(ratio2)))
 
 
 def calculateFundCost(supply, balance, ratios, amount):
-    return Decimal(balance)*((Decimal(supply+amount)/Decimal(supply))**(1000000/Decimal(ratios))-1)
+    supply, balance, ratios, amount = [Decimal(value) for value in vars().values()]
+    return (balance)*(((supply+amount)/(supply))**(1000000/(ratios))-1)
 
 
 def calculateLiquidateReturn(supply, balance, ratios, amount):
-    return Decimal(balance)*(1-(Decimal(supply-amount)/Decimal(supply))**(1000000/Decimal(ratios)))
+    supply, balance, ratios, amount = [Decimal(value) for value in vars().values()]
+    return (balance)*(1-((supply-amount)/(supply))**(1000000/(ratios)))
 
 
 def power(baseN, baseD, expN, expD, precision):
-    return (Decimal(baseN)/Decimal(baseD))**(Decimal(expN)/Decimal(expD))*2**precision
+    baseN, baseD, expN, expD, precision = [Decimal(value) for value in vars().values()]
+    return ((baseN)/(baseD))**((expN)/(expD))*2**precision
