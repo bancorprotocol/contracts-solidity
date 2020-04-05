16690010f964ab9d1b8969c4ab98c9e7cb21e878
barakman <barakman@yahoo.com>
2020-04-05 22:18:57 +0300

    Reorganize BancorConverterLiquidity.js.

--- solidity/test/BancorConverterLiquidity.js
+++ solidity/test/BancorConverterLiquidity.js
@@ -8,19 +8,20 @@ const BancorFormula = artifacts.require('BancorFormula');
 const ContractFeatures = artifacts.require('ContractFeatures');
 const ContractRegistry = artifacts.require('ContractRegistry');
 
-const MAXVAL = web3.toBigNumber(-1);
+const MAX = web3.toBigNumber(-1);
+const LIQUIDITIES = [1000000000, 1000000, 2000000, 3000000, 4000000];
 
-async function initLiquidityPool(owner, reserveSettings) {
+async function initLiquidityPool(hasETH, ...ratios) {
     const smartToken = await SmartToken.new('name', 'symbol', 0);
     const converter = await BancorConverter.new(smartToken.address, contractRegistry.address, 0, utils.zeroAddress, 0);
 
-    for (const reserveSetting of reserveSettings) {
-        if (reserveSetting.isETH) {
-            await converter.addETHReserve(reserveSetting.ratio);
+    for (let i = 0; i < ratios.length; i++) {
+        if (hasETH && i == ratios.length - 1) {
+            await converter.addETHReserve(ratios[i] * 10000);
         }
         else {
-            const reserveToken = await ERC20Token.new('name', 'symbol', 0, MAXVAL);
-            await converter.addReserve(reserveToken.address, reserveSetting.ratio);
+            const reserveToken = await ERC20Token.new('name', 'symbol', 0, MAX);
+            await converter.addReserve(reserveToken.address, ratios[i] * 10000);
         }
     }
 
@@ -32,72 +33,46 @@ async function initLiquidityPool(owner, reserveSettings) {
 
 contract('BancorConverterLiquidity', accounts => {
     before(async () => {
+        bancorFormula = await BancorFormula.new();
+        contractFeatures = await ContractFeatures.new();
         contractRegistry = await ContractRegistry.new();
-
-        const bancorFormula = await BancorFormula.new();
-        const contractFeatures = await ContractFeatures.new();
 
         await contractRegistry.registerAddress(ContractRegistryClient.BANCOR_FORMULA, bancorFormula.address);
         await contractRegistry.registerAddress(ContractRegistryClient.CONTRACT_FEATURES, contractFeatures.address);
     });
 
-    for (const isETH of [false]) {
-        for (const ratio of [10, 20, 30, 40, 50, 60, 70, 80, 90]) {
-            it(`isETH = ${isETH}, ratio1 = ${ratio}%, ratio2 = ${100 - ratio}%`, async () => {
-                const reserveSettings = [
-                    {isETH: false, ratio: ratio * 10000},
-                    {isETH: isETH, ratio: (100 - ratio) * 10000},
-                ];
+    for (const hasETH of [false])
+        for (const ratio1 of [10, 20, 30, 40, 50, 60, 70, 80, 90])
+            test(hasETH, ratio1, 100 - ratio1);
 
-                const [converter, smartToken] = await initLiquidityPool(accounts[0], reserveSettings);
-                const reserveTokens = await Promise.all(reserveSettings.map((reserveSetting, i) => converter.reserveTokens(i)));
-                await Promise.all(reserveTokens.map(reserveToken => ERC20Token.at(reserveToken).approve(converter.address, MAXVAL)));
+    for (const hasETH of [false])
+        for (const ratio1 of [10, 20, 30, 40])
+            for (const ratio2 of [10, 20, 30, 40])
+                test(hasETH, ratio1, ratio2, 100 - ratio1 - ratio2);
 
-                let total = 0;
+    function test(hasETH, ...ratios) {
+        it(`hasETH = ${hasETH}, ratios = [${ratios.join('%, ')}%]`, async () => {
+            const [converter, smartToken] = await initLiquidityPool(hasETH, ...ratios);
+            const reserveTokens = await Promise.all(ratios.map((ratio, i) => converter.reserveTokens(i)));
+            await Promise.all(reserveTokens.map(reserveToken => ERC20Token.at(reserveToken).approve(converter.address, MAX)));
 
-                for (const liquidity of [1000000000, 1000000, 2000000, 3000000, 4000000]) {
-                    await converter.addLiquidity(reserveTokens, reserveTokens.map(reserveToken => liquidity), {value: liquidity * isETH});
-                    const balances = await Promise.all(reserveTokens.map(reserveToken => getReserveBalance(reserveToken, converter)));
-                    const balance = await smartToken.balanceOf(accounts[0]);
+            let total = 0;
 
-                    total += liquidity;
-                    assert(balance.equals(total), `owner balance in the smart token: expected ${total} but got ${balance}`);
-                    for (let i = 0; i < balances.length; i++)
-                        assert(balance.equals(total), `converter balance in reserve token #${i + 1}: expected ${total} but got ${balances[i]}`);
-                }
-            });
-        }
-    }
+            for (const liquidity of LIQUIDITIES) {
+                await converter.addLiquidity(reserveTokens, reserveTokens.map(reserveToken => liquidity), {value: liquidity * hasETH});
+                const balances = await Promise.all(reserveTokens.map(reserveToken => getReserveBalance(reserveToken, converter)));
+                const balance = await smartToken.balanceOf(accounts[0]);
 
-    for (const isETH of [false]) {
-        for (const ratio1 of [10, 20, 30, 40]) {
-            for (const ratio2 of [10, 20, 30, 40]) {
-                it(`isETH = ${isETH}, ratio1 = ${ratio1}%, ratio2 = ${ratio2}%, ratio3 = ${100 - ratio1 - ratio2}%`, async () => {
-                    const reserveSettings = [
-                        {isETH: false, ratio: ratio1 * 10000},
-                        {isETH: false, ratio: ratio2 * 10000},
-                        {isETH: isETH, ratio: (100 - ratio1 - ratio2) * 10000},
-                    ];
+                if (total == 0)
+                    total = liquidity;
+                else
+                    total += Math.floor(liquidity * Math.min(...ratios) / 100);
 
-                    const [converter, smartToken] = await initLiquidityPool(accounts[0], reserveSettings);
-                    const reserveTokens = await Promise.all(reserveSettings.map((reserveSetting, i) => converter.reserveTokens(i)));
-                    await Promise.all(reserveTokens.map(reserveToken => ERC20Token.at(reserveToken).approve(converter.address, MAXVAL)));
-
-                    let total = 0;
-
-                    for (const liquidity of [1000000000, 1000000, 2000000, 3000000, 4000000]) {
-                        await converter.addLiquidity(reserveTokens, reserveTokens.map(reserveToken => liquidity), {value: liquidity * isETH});
-                        const balances = await Promise.all(reserveTokens.map(reserveToken => getReserveBalance(reserveToken, converter)));
-                        const balance = await smartToken.balanceOf(accounts[0]);
-
-                        total += liquidity;
-                        assert(balance.equals(total), `owner balance in the smart token: expected ${total} but got ${balance}`);
-                        for (let i = 0; i < balances.length; i++)
-                            assert(balance.equals(total), `converter balance in reserve token #${i + 1}: expected ${total} but got ${balances[i]}`);
-                    }
-                });
+                assert(balance.equals(total), `owner balance in the smart token: expected ${total} but got ${balance}`);
+                for (let i = 0; i < balances.length; i++)
+                    assert(balance.equals(total), `converter balance in reserve token #${i + 1}: expected ${total} but got ${balances[i]}`);
             }
-        }
+        });
     }
 
     async function getReserveBalance(reserveToken, converter) {
