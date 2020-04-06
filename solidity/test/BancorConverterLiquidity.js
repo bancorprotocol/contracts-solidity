55f9287f7574c52d16c3ddb654e8339219bad500
barakman <barakman@yahoo.com>
2020-04-06 20:04:10 +0300

    Cosmetic.

--- solidity/test/BancorConverterLiquidity.js
+++ solidity/test/BancorConverterLiquidity.js
@@ -79,52 +79,52 @@ contract('BancorConverterLiquidity', accounts => {
     });
 
     describe('function addLiquidity:', () => {
-    for (const hasETH of [false, true])
-        for (const ratio1 of [10, 20, 30, 40, 50, 60, 70, 80, 90])
-            for (const ratio2 of [10, 20, 30, 40, 50, 60, 70, 80, 90])
-                if (ratio1 + ratio2 <= 100)
-                    test(hasETH, ratio1, ratio2);
+        for (const hasETH of [false, true])
+            for (const ratio1 of [10, 20, 30, 40, 50, 60, 70, 80, 90])
+                for (const ratio2 of [10, 20, 30, 40, 50, 60, 70, 80, 90])
+                    if (ratio1 + ratio2 <= 100)
+                        test(hasETH, ratio1, ratio2);
 
-    for (const hasETH of [false, true])
-        for (const ratio1 of [10, 20, 30, 40, 50, 60])
-            for (const ratio2 of [10, 20, 30, 40, 50, 60])
-                for (const ratio3 of [10, 20, 30, 40, 50, 60])
-                    if (ratio1 + ratio2 + ratio3 <= 100)
-                        test(hasETH, ratio1, ratio2, ratio3);
+        for (const hasETH of [false, true])
+            for (const ratio1 of [10, 20, 30, 40, 50, 60])
+                for (const ratio2 of [10, 20, 30, 40, 50, 60])
+                    for (const ratio3 of [10, 20, 30, 40, 50, 60])
+                        if (ratio1 + ratio2 + ratio3 <= 100)
+                            test(hasETH, ratio1, ratio2, ratio3);
 
-    function test(hasETH, ...ratios) {
-        it(`hasETH = ${hasETH}, ratios = [${ratios.join('%, ')}%]`, async () => {
-            const [converter, smartToken] = await initLiquidityPool(hasETH, ...ratios);
-            const reserveTokens = await Promise.all(ratios.map((ratio, i) => converter.reserveTokens(i)));
+        function test(hasETH, ...ratios) {
+            it(`hasETH = ${hasETH}, ratios = [${ratios.join('%, ')}%]`, async () => {
+                const [converter, smartToken] = await initLiquidityPool(hasETH, ...ratios);
+                const reserveTokens = await Promise.all(ratios.map((ratio, i) => converter.reserveTokens(i)));
 
-            let expected = [];
-            const minDiff = ratios.reduce((a, b) => a + b, 0) == 100 ? "1" : "0.996";
-            const maxDiff = ratios.reduce((a, b) => a + b, 0) == 100 ? "0" : "0.002";
+                let expected = [];
+                const minDiff = ratios.reduce((a, b) => a + b, 0) == 100 ? "1" : "0.996";
+                const maxDiff = ratios.reduce((a, b) => a + b, 0) == 100 ? "0" : "0.002";
 
-            for (const liquidity of [1000000000, 1000000, 2000000, 3000000, 4000000]) {
-                const reserveAmounts = reserveTokens.map((reserveToken, i) => web3.toBigNumber(liquidity).mul(100 + i).div(100));
-                await Promise.all(reserveTokens.map((reserveToken, i) => approve(reserveToken, converter, reserveAmounts[i].mul(0))));
-                await Promise.all(reserveTokens.map((reserveToken, i) => approve(reserveToken, converter, reserveAmounts[i].mul(1))));
-                await converter.addLiquidity(reserveTokens, reserveAmounts, {value: hasETH ? reserveAmounts.slice(-1)[0] : 0});
-                const allowances = await Promise.all(reserveTokens.map(reserveToken => getAllowance(reserveToken, converter)));
-                const balances = await Promise.all(reserveTokens.map(reserveToken => getBalance(reserveToken, converter)));
-                const supply = await smartToken.balanceOf(owner);
+                for (const liquidity of [1000000000, 1000000, 2000000, 3000000, 4000000]) {
+                    const reserveAmounts = reserveTokens.map((reserveToken, i) => web3.toBigNumber(liquidity).mul(100 + i).div(100));
+                    await Promise.all(reserveTokens.map((reserveToken, i) => approve(reserveToken, converter, reserveAmounts[i].mul(0))));
+                    await Promise.all(reserveTokens.map((reserveToken, i) => approve(reserveToken, converter, reserveAmounts[i].mul(1))));
+                    await converter.addLiquidity(reserveTokens, reserveAmounts, {value: hasETH ? reserveAmounts.slice(-1)[0] : 0});
+                    const allowances = await Promise.all(reserveTokens.map(reserveToken => getAllowance(reserveToken, converter)));
+                    const balances = await Promise.all(reserveTokens.map(reserveToken => getBalance(reserveToken, converter)));
+                    const supply = await smartToken.balanceOf(owner);
 
-                for (let i = 0; i < allowances.length; i++) {
-                    const diff = allowances[i].div(reserveAmounts[i]);
-                    assert(diff.lessThanOrEqualTo(maxDiff), `allowance #${i + 1}: diff = ${diff.toFixed()}`);
+                    for (let i = 0; i < allowances.length; i++) {
+                        const diff = allowances[i].div(reserveAmounts[i]);
+                        assert(diff.lessThanOrEqualTo(maxDiff), `allowance #${i + 1}: diff = ${diff.toFixed()}`);
+                    }
+
+                    const actual = balances.map(balance => balance.div(supply));
+                    for (let i = 0; i < expected.length; i++) {
+                        const diff = expected[i].div(actual[i]);
+                        assert(diff.greaterThanOrEqualTo(minDiff) && diff.lessThanOrEqualTo("1"), `balance #${i + 1}: diff = ${diff.toFixed()}`);
+                    }
+
+                    expected = actual;
                 }
-
-                const actual = balances.map(balance => balance.div(supply));
-                for (let i = 0; i < expected.length; i++) {
-                    const diff = expected[i].div(actual[i]);
-                    assert(diff.greaterThanOrEqualTo(minDiff) && diff.lessThanOrEqualTo("1"), `balance #${i + 1}: diff = ${diff.toFixed()}`);
-                }
-
-                expected = actual;
-            }
-        });
-    }
+            });
+        }
     });
 
     async function approve(reserveToken, converter, amount) {
