377226287cb0df5f76f8ef6dcf1efa4fab2ace67
barakman <barakman@yahoo.com>
2020-05-04 20:44:06 +0300

    Revert "Minor coding improvement in BancorConverter."
    This reverts commit 8f5663e186f2599f6234cc72619958eb7241d794.

--- solidity/contracts/converter/BancorConverter.sol
+++ solidity/contracts/converter/BancorConverter.sol
@@ -903,9 +903,10 @@ contract BancorConverter is IBancorConverter, TokenHandler, SmartTokenController
         if (msg.value > 0)
             require(reserves[ETH_RESERVE_ADDRESS].isSet);
 
-        uint256 supplyAmount = addLiquidityToPool(_reserveTokens, _reserveAmounts, token.totalSupply());
-        require(supplyAmount >= _supplyMinReturnAmount);
+        uint256 totalSupply = token.totalSupply();
+        uint256 supplyAmount = addLiquidityToPool(_reserveTokens, _reserveAmounts, totalSupply);
 
+        require(supplyAmount >= _supplyMinReturnAmount);
         token.issue(msg.sender, supplyAmount);
     }
 
@@ -924,9 +925,10 @@ contract BancorConverter is IBancorConverter, TokenHandler, SmartTokenController
     {
         verifyLiquidityInput(_reserveTokens, _reserveMinReturnAmounts, _supplyAmount);
 
+        uint256 totalSupply = token.totalSupply();
         token.destroy(msg.sender, _supplyAmount);
 
-        removeLiquidityFromPool(_reserveTokens, _reserveMinReturnAmounts, token.totalSupply(), _supplyAmount);
+        removeLiquidityFromPool(_reserveTokens, _reserveMinReturnAmounts, totalSupply, _supplyAmount);
     }
 
     function verifyLiquidityInput(IERC20Token[] memory _reserveTokens, uint256[] memory _reserveAmounts, uint256 _supplyAmount)
