a5bbd7b2285c01d68558bbf205ce105219ed2826
Leonid Beder <leonid.beder@gmail.com>
2020-06-23 15:08:48 +0300

    Move BN

--- solidity/test/ERC20Token.js
+++ solidity/test/ERC20Token.js
@@ -1,7 +1,6 @@
 import { expect } from 'chai';
 import { constants } from '@openzeppelin/test-helpers';
-import { expectRevert, expectEvent } from '@openzeppelin/test-helpers';
-import { BN } from '@openzeppelin/test-helpers/src/setup';
+import { expectRevert, expectEvent, BN } from '@openzeppelin/test-helpers';
 
 const ERC20Token = artifacts.require('ERC20Token');
 
