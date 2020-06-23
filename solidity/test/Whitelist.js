73bcdf9488f962ad807300bacf483d5cddc3bf7e
Leonid Beder <leonid.beder@gmail.com>
2020-06-23 17:31:36 +0300

    Refactor Whitelist.js

--- solidity/test/Whitelist.js
+++ solidity/test/Whitelist.js
@@ -1,119 +1,102 @@
-/* global artifacts, contract, it, assert */
-/* eslint-disable prefer-reflect */
+import { expect } from 'chai';
+import { constants } from '@openzeppelin/test-helpers';
+import { expectRevert, expectEvent } from '@openzeppelin/test-helpers';
 
 const Whitelist = artifacts.require('Whitelist');
-const utils = require('./helpers/Utils');
-
-const invalidAccount = '0x0';
 
 contract('Whitelist', accounts => {
+    let whitelist;
+    const address1 = accounts[1];
+    const address2 = accounts[2];
+    const address3 = accounts[3];
+    const nonOwner = accounts[8];
+
+    beforeEach(async () => {
+        whitelist = await Whitelist.new();
+    });
+
     it('verifies that a given address is not whitelisted after construction', async () => {
-        let whitelist = await Whitelist.new();
-        let isWhitelisted = await whitelist.isWhitelisted.call(accounts[1]);
-        assert(!isWhitelisted);
+        expect(await whitelist.isWhitelisted.call(address1)).to.be.false();
     });
 
     it('verifies that the owner can add an address to the whitelist', async () => {
-        let whitelist = await Whitelist.new();
-        await whitelist.addAddress(accounts[1]);
-        let isWhitelisted = await whitelist.isWhitelisted.call(accounts[1]);
-        assert(isWhitelisted);
+        await whitelist.addAddress(address1);
+
+        expect(await whitelist.isWhitelisted.call(address1)).to.be.true();
     });
 
-    it('should throw when a non owner tries to add an address to the whitelist', async () => {
-        let whitelist = await Whitelist.new();
-
-        await utils.catchRevert(whitelist.addAddress(accounts[1], { from: accounts[2] }));
+    it('should revert when a non owner tries to add an address to the whitelist', async () => {
+        await expectRevert(whitelist.addAddress(address1, { from: nonOwner }), 'ERR_ACCESS_DENIED');
     });
 
-    it('should throw when the owner tries to add an invalid address to the whitelist', async () => {
-        let whitelist = await Whitelist.new();
-
-        await utils.catchRevert(whitelist.addAddress(invalidAccount));
+    it('should revert when the owner tries to add an invalid address to the whitelist', async () => {
+        await expectRevert(whitelist.addAddress(constants.ZERO_ADDRESS), 'ERR_INVALID_ADDRESS');
     });
 
     it('verifies that the owner can add multiple addresses to the whitelist', async () => {
-        let whitelist = await Whitelist.new();
-        await whitelist.addAddresses([accounts[1], accounts[2]]);
-        let isWhitelisted = await whitelist.isWhitelisted.call(accounts[1]);
-        assert(isWhitelisted);
-        isWhitelisted = await whitelist.isWhitelisted.call(accounts[2]);
-        assert(isWhitelisted);
+        await whitelist.addAddresses([address1, address2]);
+
+        expect(await whitelist.isWhitelisted.call(address1)).to.be.true();
+        expect(await whitelist.isWhitelisted.call(address2)).to.be.true();
     });
 
-    it('should throw when a non owner tries to add multiple addresses to the whitelist', async () => {
-        let whitelist = await Whitelist.new();
-
-        await utils.catchRevert(whitelist.addAddresses([accounts[1], accounts[2]], { from: accounts[2] }));
+    it('should revert when a non owner tries to add multiple addresses to the whitelist', async () => {
+        await expectRevert(whitelist.addAddresses([address1, address2], { from: nonOwner }), 'ERR_ACCESS_DENIED');
     });
 
     it('verifies that the owner can remove an address from the whitelist', async () => {
-        let whitelist = await Whitelist.new();
-        await whitelist.addAddress(accounts[1]);
-        let isWhitelisted = await whitelist.isWhitelisted.call(accounts[1]);
-        assert(isWhitelisted);
+        await whitelist.addAddress(address1);
+        expect(await whitelist.isWhitelisted.call(address1)).to.be.true();
 
-        await whitelist.removeAddress(accounts[1]);
-        isWhitelisted = await whitelist.isWhitelisted.call(accounts[1]);
-        assert(!isWhitelisted);
+        await whitelist.removeAddress(address1);
+        expect(await whitelist.isWhitelisted.call(address1)).to.be.false();
     });
 
-    it('should throw when a non owner tries to remove an address from the whitelist', async () => {
-        let whitelist = await Whitelist.new();
-        await whitelist.addAddress(accounts[1]);
-        let isWhitelisted = await whitelist.isWhitelisted.call(accounts[1]);
-        assert(isWhitelisted);
+    it('should revert when a non owner tries to remove an address from the whitelist', async () => {
+        await whitelist.addAddress(address1);
+        expect(await whitelist.isWhitelisted.call(address1)).to.be.true();
 
-        await utils.catchRevert(whitelist.removeAddress(accounts[1], { from: accounts[2] }));
+        await expectRevert(whitelist.removeAddress(address1, { from: nonOwner }), 'ERR_ACCESS_DENIED');
     });
 
     it('verifies that the owner can remove multiple addresses from the whitelist', async () => {
-        let whitelist = await Whitelist.new();
-        await whitelist.addAddresses([accounts[1], accounts[2], accounts[3]]);
-        let isWhitelisted = await whitelist.isWhitelisted.call(accounts[1]);
-        assert(isWhitelisted);
-        isWhitelisted = await whitelist.isWhitelisted.call(accounts[2]);
-        assert(isWhitelisted);
-        isWhitelisted = await whitelist.isWhitelisted.call(accounts[3]);
-        assert(isWhitelisted);
+        await whitelist.addAddresses([address1, address2, address3]);
+        expect(await whitelist.isWhitelisted.call(address1)).to.be.true();
+        expect(await whitelist.isWhitelisted.call(address2)).to.be.true();
+        expect(await whitelist.isWhitelisted.call(address3)).to.be.true();
 
-        await whitelist.removeAddresses([accounts[1], accounts[3]]);
-        isWhitelisted = await whitelist.isWhitelisted.call(accounts[1]);
-        assert(!isWhitelisted);
-        isWhitelisted = await whitelist.isWhitelisted.call(accounts[2]);
-        assert(isWhitelisted);
-        isWhitelisted = await whitelist.isWhitelisted.call(accounts[3]);
-        assert(!isWhitelisted);
+        await whitelist.removeAddresses([address1, address3]);
+        expect(await whitelist.isWhitelisted.call(address1)).to.be.false();
+        expect(await whitelist.isWhitelisted.call(address2)).to.be.true();
+        expect(await whitelist.isWhitelisted.call(address3)).to.be.false();
     });
 
-    it('should throw when a non owner tries to remove multiple address from the whitelist', async () => {
-        let whitelist = await Whitelist.new();
-        await whitelist.addAddresses([accounts[1], accounts[2], accounts[3]]);
-        let isWhitelisted = await whitelist.isWhitelisted.call(accounts[1]);
-        assert(isWhitelisted);
-        isWhitelisted = await whitelist.isWhitelisted.call(accounts[2]);
-        assert(isWhitelisted);
-        isWhitelisted = await whitelist.isWhitelisted.call(accounts[3]);
-        assert(isWhitelisted);
+    it('should revert when a non owner tries to remove multiple address from the whitelist', async () => {
+        await whitelist.addAddresses([address1, address2, address3]);
+        expect(await whitelist.isWhitelisted.call(address1)).to.be.true();
+        expect(await whitelist.isWhitelisted.call(address2)).to.be.true();
+        expect(await whitelist.isWhitelisted.call(address3)).to.be.true();
 
-        await utils.catchRevert(whitelist.removeAddresses([accounts[1], accounts[3]], { from: accounts[2] }));
+        await expectRevert(whitelist.removeAddresses([address1, address3], { from: nonOwner }), 'ERR_ACCESS_DENIED');
     });
 
     it('verifies that an address can be added unless it is already added and removed unless it is already removed', async () => {
-        let whitelist = await Whitelist.new();
-        let status0 = await whitelist.isWhitelisted.call(accounts[1]);
-        let response1 = await whitelist.addAddress(accounts[1]);
-        let status1 = await whitelist.isWhitelisted.call(accounts[1]);
-        let response2 = await whitelist.addAddress(accounts[1]);
-        let status2 = await whitelist.isWhitelisted.call(accounts[1]);
-        let response3 = await whitelist.removeAddress(accounts[1]);
-        let status3 = await whitelist.isWhitelisted.call(accounts[1]);
-        let response4 = await whitelist.removeAddress(accounts[1]);
-        let status4 = await whitelist.isWhitelisted.call(accounts[1]);
-        assert(!status0 && status1 && status2 && !status3 && !status4);
-        assert(response1.logs.length == 1 && response1.logs[0].event == "AddressAddition" && response1.logs[0].args._address == accounts[1]);
-        assert(response2.logs.length == 0);
-        assert(response3.logs.length == 1 && response3.logs[0].event == "AddressRemoval" && response3.logs[0].args._address == accounts[1]);
-        assert(response4.logs.length == 0);
+        expect(await whitelist.isWhitelisted.call(address1)).to.be.false();
+
+        const res1 = await whitelist.addAddress(address1);
+        expectEvent(res1, 'AddressAddition', { _address: address1 });
+        expect(await whitelist.isWhitelisted.call(address1)).to.be.true();
+
+        const res2 = await whitelist.addAddress(address1);
+        expectEvent.notEmitted(res2, 'AddressAddition');
+        expect(await whitelist.isWhitelisted.call(address1)).to.be.true();
+
+        const res3 = await whitelist.removeAddress(address1);
+        expectEvent(res3, 'AddressRemoval', { _address: address1 });
+        expect(await whitelist.isWhitelisted.call(address1)).to.be.false();
+
+        const res4 = await whitelist.removeAddress(address1);
+        expectEvent.notEmitted(res4, 'AddressRemoval');
+        expect(await whitelist.isWhitelisted.call(address1)).to.be.false();
     });
 });
