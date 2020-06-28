271698c567a44ffd7a3cbf2206967be6b5e2944d
Leonid Beder <leonid.beder@gmail.com>
2020-06-28 04:20:42 +0300

    Remove deprecated option

--- .solcover.js
+++ .solcover.js
@@ -1,7 +1,6 @@
 module.exports = {
     testCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle run coverage',
     compileCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle compile',
-    norpc: true,
     skipFiles: [
         'helpers/Migrations.sol'
     ],
