f7a3f10e5adcc73790c6e5e1a09e41933bc0d4f1
Leonid Beder <leonid.beder@gmail.com>
2020-06-28 14:32:36 +0300

    Avoid coverage report for test helpers

--- .solcover.js
+++ .solcover.js
@@ -2,7 +2,7 @@ module.exports = {
     testCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle run coverage',
     compileCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle compile',
     skipFiles: [
-        'helpers/Migrations.sol'
+        'contracts/helpers'
     ],
     providerOptions: {
         default_balance_ether: 10000000000000000000
