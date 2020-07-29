b3e6eb238f68b3694cde385270aad5a9e467c041
Leonid Beder <leonid.beder@gmail.com>
2020-07-29 10:49:48 +0300

    Style

--- solidity/test/LiquidityPoolV2Converter.js
+++ solidity/test/LiquidityPoolV2Converter.js
@@ -145,7 +145,7 @@ contract('LiquidityPoolV2Converter', accounts => {
                 const primaryReserveData = isReserve1Primary ? reserve1Data : reserve2Data;
                 const secondaryReserveData = isReserve1Primary ? reserve2Data : reserve1Data;
 
-                let newWeights = balancedWeights(
+                const newWeights = balancedWeights(
                     primaryReserveData[0].mul(AMPLIFICATION_FACTOR),
                     primaryReserveData[1],
                     secondaryReserveData[1],
@@ -157,10 +157,12 @@ contract('LiquidityPoolV2Converter', accounts => {
                 if (isReserve1Primary) {
                     const x = reserve1StakedBalance.mul(rate.n).mul(weights[1]);
                     const y = reserve2StakedBalance.mul(rate.d).mul(weights[0]);
-                    if (x.mul(AMPLIFICATION_FACTOR).gte(y.mul(AMPLIFICATION_FACTOR.add(new BN(1)))))
+                    if (x.mul(AMPLIFICATION_FACTOR).gte(y.mul(AMPLIFICATION_FACTOR.add(new BN(1))))) {
                         return [weights, conversionFee.div(new BN(2))];
-                    if (x.mul(AMPLIFICATION_FACTOR.mul(new BN(2))).lte(y.mul(AMPLIFICATION_FACTOR.mul(new BN(2)).sub(new BN(1)))))
+                    }
+                    if (x.mul(AMPLIFICATION_FACTOR.mul(new BN(2))).lte(y.mul(AMPLIFICATION_FACTOR.mul(new BN(2)).sub(new BN(1))))) {
                         return [weights, conversionFee.mul(new BN(2))];
+                    }
                     return [weights, conversionFee.mul(y).div(x.mul(AMPLIFICATION_FACTOR).sub(y.mul(AMPLIFICATION_FACTOR.sub(new BN(1)))))];
                 }
                 return [[weights[1], weights[0]], conversionFee];
