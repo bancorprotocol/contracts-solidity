16b55f2c9d5493afef71926d32b1a0db5978ae92
barakman <barakman@yahoo.com>
2020-08-07 10:42:51 +0300

    Reduce running-time of the tests of function `calculateFeeToEquilibrium`.

--- solidity/test/LiquidityPoolV2ConverterStateless.js
+++ solidity/test/LiquidityPoolV2ConverterStateless.js
@@ -11,31 +11,25 @@ contract('LiquidityPoolV2ConverterStateless', accounts => {
         converter = await LiquidityPoolV2Converter.new(DUMMY_ADDRESS, DUMMY_ADDRESS, 0);
     });
 
-    describe('function calculateFeeToEquilibrium', () => {
+    describe('dynamic-fee', () => {
         const AMPLIFICATION_FACTOR = new BN(20);
-        const WEIGHT_RESOLUTION = new BN(1000000);
-        const stakedValues = [123456, 234567, 345678].map(x => AMPLIFICATION_FACTOR.mul(new BN(x)));
-        const weightValues = [20, 35, 50, 65, 80].map(x => new BN(x * 10000));
-        const rateValues   = [1234, 3456, 5678, 7890].map(x => new BN(x));
-        const factorValues = [1, 2, 3, 4, 5].map(x => new BN(x * 10000));
+        const stakedValues = [1234, 2345, 3456, 4567, 5678, 6789].map(x => AMPLIFICATION_FACTOR.mul(new BN(x)));
+        const factorValues = [0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0].map(x => new BN(x * 10000));
+        const tknWeight = new BN(1);
+        const bntWeight = new BN(1);
+        const tknRate = new BN(1);
+        const bntRate = new BN(1);
 
         for (const tknStaked of stakedValues) {
             for (const bntStaked of stakedValues) {
-                for (const tknWeight of weightValues) {
-                    for (const tknRate of rateValues) {
-                        for (const bntRate of rateValues) {
-                            for (const feeFactor of factorValues) {
-                                const bntWeight = WEIGHT_RESOLUTION.sub(tknWeight);
-                                const x = tknStaked.mul(tknRate).mul(bntWeight);
-                                const y = bntStaked.mul(bntRate).mul(tknWeight);
-                                const expected = y.gt(x) ? y.sub(x).mul(feeFactor).mul(AMPLIFICATION_FACTOR).div(y) : new BN(0);
-                                it(`should return ${expected.toString()}`, async () => {
-                                    const actual = await converter.calculateFeeToEquilibriumTest.call(tknStaked, bntStaked, tknWeight, bntWeight, tknRate, bntRate, feeFactor);
-                                    expect(actual).to.be.bignumber.equal(expected);
-                                });
-                            }
-                        }
-                    }
+                for (const feeFactor of factorValues) {
+                    const x = tknStaked.mul(tknRate).mul(bntWeight);
+                    const y = bntStaked.mul(bntRate).mul(tknWeight);
+                    const expected = y.gt(x) ? y.sub(x).mul(feeFactor).mul(AMPLIFICATION_FACTOR).div(y) : new BN(0);
+                    it(`calculateFeeToEquilibrium(${[tknStaked, bntStaked, tknWeight, bntWeight, tknRate, bntRate, feeFactor]}) should return ${expected.toString()}`, async () => {
+                        const actual = await converter.calculateFeeToEquilibriumTest.call(tknStaked, bntStaked, tknWeight, bntWeight, tknRate, bntRate, feeFactor);
+                        expect(actual).to.be.bignumber.equal(expected);
+                    });
                 }
             }
         }
