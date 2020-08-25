ca9dd2a6332f6c4cf0bddd9329098e09fc094b72
yudilevi <yudi@bancor.network>
2020-08-25 11:00:11 +0300

    cosmetic

--- scripts/flatten.js
+++ scripts/flatten.js
@@ -36,7 +36,7 @@ const getSourceCode = (pathName) => {
     // removing all occurrences of SPDX license identifiers except first
     // TODO: this is only ok if all files have the same license
     let i = 0;
-    const source = result.output.toString().replace(/\/\/ SPDX-License-Identifier.*/g, m  => !i++ ? m : '');
+    const source = result.output.toString().replace(/\/\/ SPDX-License-Identifier.*/g, m => !i++ ? m : '');
     return source.slice(1, -1);
 };
 
