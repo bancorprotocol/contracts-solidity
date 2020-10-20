b151a22f2ff02cfaadb2bbc962d1c0e2cc811e44
barakman <barakman@yahoo.com>
2020-10-20 22:15:43 +0300

    Upgrade converter version.

--- solidity/contracts/converter/ConverterBase.sol
+++ solidity/contracts/converter/ConverterBase.sol
@@ -54,7 +54,7 @@ abstract contract ConverterBase is IConverter, TokenHandler, TokenHolder, Contra
     /**
       * @dev version number
     */
-    uint16 public constant version = 42;
+    uint16 public constant version = 43;
 
     IConverterAnchor public override anchor;            // converter anchor contract
     IWhitelist public override conversionWhitelist;     // whitelist contract with list of addresses that are allowed to use the converter
