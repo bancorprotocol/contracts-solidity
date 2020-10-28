1ed069570b6e89d9a8c922a891a78eaa9a8a8448
barakman <barakman@yahoo.com>
2020-10-28 18:25:54 +0200

    Add safety measure (replace 'minus' with `sub` during the compensation calculation).

--- solidity/contracts/liquidity-protection/LiquidityProtection.sol
+++ solidity/contracts/liquidity-protection/LiquidityProtection.sol
@@ -1202,7 +1202,7 @@ contract LiquidityProtection is TokenHandler, ContractRegistryClient, Reentrancy
     function compensation(uint256 _amount, uint256 _total, Fraction memory _loss, Fraction memory _level) internal pure returns (uint256) {
         (uint256 lossN, uint256 lossD) = Math.reducedRatio(_loss.n, _loss.d, MAX_UINT128);
         (uint256 compN, uint256 compD) = Math.reducedRatio(_loss.n.mul(_level.n), _loss.d.mul(_level.d), MAX_UINT128);
-        return _total.mul(lossD - lossN).div(lossD).add(_amount.mul(compN).div(compD));
+        return _total.mul(lossD.sub(lossN)).div(lossD).add(_amount.mul(compN).div(compD));
     }
 
     /**
