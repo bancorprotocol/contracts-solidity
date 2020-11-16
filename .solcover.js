4ba31e6665dc22ec88f1c719eccf2938be35111d
barakman <barakman@yahoo.com>
2020-11-16 02:42:38 +0200

    Add `db: memdown()` to the `providerOptions` field in the solidity-coverage configuration file.

--- .solcover.js
+++ .solcover.js
@@ -1,8 +1,11 @@
+const memdown = require('memdown');
+
 module.exports = {
     testCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle run coverage',
     compileCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle compile',
     skipFiles: ['contracts/helpers'],
     providerOptions: {
+        db: memdown(),
         default_balance_ether: 10000000000000000000
     }
 };
