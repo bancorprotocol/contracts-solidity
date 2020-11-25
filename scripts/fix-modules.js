1e64809261c7869194fe77e7894f393d269571c9
barakman <barakman@yahoo.com>
2020-11-25 12:17:52 +0200

    Cosmetic.

--- scripts/fix-modules.js
+++ scripts/fix-modules.js
@@ -1,18 +1,18 @@
-const fs = require("fs");
+const fs = require('fs');
 
 function fix(fileName, tokens) {
-    console.log("Fixing " + fileName);
+    console.log('Fixing ' + fileName);
     try {
-        let data = fs.readFileSync(fileName, {encoding: "utf8"});
+        let data = fs.readFileSync(fileName, {encoding: 'utf8'});
         for (const token of tokens)
             data = data.split(token.prev).join(token.next);
-        fs.writeFileSync(fileName, data, {encoding: "utf8"});
+        fs.writeFileSync(fileName, data, {encoding: 'utf8'});
     }
     catch (error) {
         console.log(error.message);
     }
 }
 
-fix("./node_modules/@openzeppelin/test-environment/node_modules/ganache-core/node_modules/deferred-leveldown/deferred-leveldown.js",
-    [{prev: "function DeferredLevelDOWN (db) {", next: "function DeferredLevelDOWN(db) {db.db = require('memdown')();"}]
+fix('./node_modules/@openzeppelin/test-environment/node_modules/ganache-core/node_modules/deferred-leveldown/deferred-leveldown.js',
+    [{prev: 'function DeferredLevelDOWN (db) {', next: 'function DeferredLevelDOWN(db) {db.db = require(\'memdown\')();'}]
 );
