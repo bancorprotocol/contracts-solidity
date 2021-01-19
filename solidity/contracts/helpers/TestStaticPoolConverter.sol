5647fdba856812d0295f10c14e6124e4ca50dc4e
barakman <barakman@yahoo.com>
2021-01-19 21:27:31 +0200

    Add TestStaticPoolConverter.sol.

---
+++ solidity/contracts/helpers/TestStaticPoolConverter.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity 0.6.12;
+
+import "./TestStandardPoolConverter.sol";
+
+contract TestStaticPoolConverter is TestStandardPoolConverter {
+    constructor(
+        IDSToken _token,
+        IContractRegistry _registry,
+        uint32 _maxConversionFee
+    ) public TestStandardPoolConverter(_token, _registry, _maxConversionFee) {}
+}
