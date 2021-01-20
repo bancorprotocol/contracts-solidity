79b91a25cd62ab15675ef004d55f329a91d84b01
barakman <barakman@yahoo.com>
2021-01-20 21:40:40 +0200

    Fix test_deployment.js.

--- solidity/utils/test_deployment.js
+++ solidity/utils/test_deployment.js
@@ -388,7 +388,8 @@ const run = async () => {
             bntTokenGovernance._address,
             vbntTokenGovernance._address,
             checkpointStore._address
-        ]
+        ],
+        0
     ]);
 
     await execute(checkpointStore.methods.grantRole(ROLE_OWNER, liquidityProtection._address));
