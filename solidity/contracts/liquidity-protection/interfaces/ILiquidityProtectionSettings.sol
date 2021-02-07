f91c89a7b4ed2b59c99068d5e1606864e284a6e0
barakman <barakman@yahoo.com>
2021-02-07 18:24:40 +0200

    Cosmetic.

--- solidity/contracts/liquidity-protection/interfaces/ILiquidityProtectionSettings.sol
+++ solidity/contracts/liquidity-protection/interfaces/ILiquidityProtectionSettings.sol
@@ -9,47 +9,47 @@ import "../../token/interfaces/IERC20Token.sol";
     Liquidity Protection Store Settings interface
 */
 interface ILiquidityProtectionSettings {
-    function addPoolToWhitelist(IConverterAnchor _poolAnchor) external;
+    function addPoolToWhitelist(IConverterAnchor poolAnchor) external;
 
-    function removePoolFromWhitelist(IConverterAnchor _poolAnchor) external;
+    function removePoolFromWhitelist(IConverterAnchor poolAnchor) external;
 
-    function isPoolWhitelisted(IConverterAnchor _poolAnchor) external view returns (bool);
+    function isPoolWhitelisted(IConverterAnchor poolAnchor) external view returns (bool);
 
     function poolWhitelist() external view returns (address[] memory);
 
-    function addSubscriber(ILiquidityProtectionEventsSubscriber _subscriber) external;
+    function addSubscriber(ILiquidityProtectionEventsSubscriber subscriber) external;
 
-    function removeSubscriber(ILiquidityProtectionEventsSubscriber _subscriber) external;
+    function removeSubscriber(ILiquidityProtectionEventsSubscriber subscriber) external;
 
     function subscribers() external view returns (address[] memory);
 
-    function isPoolSupported(IConverterAnchor _poolAnchor) external view returns (bool);
+    function isPoolSupported(IConverterAnchor poolAnchor) external view returns (bool);
 
     function minNetworkTokenLiquidityForMinting() external view returns (uint256);
 
     function defaultNetworkTokenMintingLimit() external view returns (uint256);
 
-    function networkTokenMintingLimits(IConverterAnchor _poolAnchor) external view returns (uint256);
+    function networkTokenMintingLimits(IConverterAnchor poolAnchor) external view returns (uint256);
 
-    function addLiquidityDisabled(IConverterAnchor _poolAnchor, IERC20Token _reserveToken) external view returns (bool);
+    function addLiquidityDisabled(IConverterAnchor poolAnchor, IERC20Token reserveToken) external view returns (bool);
 
-    function disableAddLiquidity(IConverterAnchor _poolAnchor, IERC20Token _reserveToken, bool _disable) external;
+    function disableAddLiquidity(IConverterAnchor poolAnchor, IERC20Token reserveToken, bool disable) external;
 
     function minProtectionDelay() external view returns (uint256);
 
     function maxProtectionDelay() external view returns (uint256);
 
-    function setProtectionDelays(uint256 _minProtectionDelay, uint256 _maxProtectionDelay) external;
+    function setProtectionDelays(uint256 minDelay, uint256 maxDelay) external;
 
     function minNetworkCompensation() external view returns (uint256);
 
-    function setMinNetworkCompensation(uint256 _minCompensation) external;
+    function setMinNetworkCompensation(uint256 amount) external;
 
     function lockDuration() external view returns (uint256);
 
-    function setLockDuration(uint256 _lockDuration) external;
+    function setLockDuration(uint256 duration) external;
 
     function averageRateMaxDeviation() external view returns (uint32);
 
-    function setAverageRateMaxDeviation(uint32 _averageRateMaxDeviation) external;
+    function setAverageRateMaxDeviation(uint32 deviation) external;
 }
