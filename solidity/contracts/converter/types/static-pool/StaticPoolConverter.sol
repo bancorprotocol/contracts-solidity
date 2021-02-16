de1ab101e8f22f2748b3365b7a73d3b072923d4a
Barak Manos <barakman@yahoo.com>
2021-02-16 13:03:47 +0200

    prev commit

--- solidity/contracts/converter/types/static-pool/StaticPoolConverter.sol
+++ solidity/contracts/converter/types/static-pool/StaticPoolConverter.sol
@@ -8,6 +8,9 @@ import "../standard-pool/StandardPoolConverter.sol";
  * liquidity pool that has 2 reserves with 50%/50% weights, and a conversion-rate of 1:1.
  */
 contract StaticPoolConverter is StandardPoolConverter {
+    uint128 private _rateN;
+    uint128 private _rateD;
+
     /**
      * @dev initializes a new StaticPoolConverter instance
      *
@@ -19,7 +22,10 @@ contract StaticPoolConverter is StandardPoolConverter {
         IConverterAnchor _anchor,
         IContractRegistry _registry,
         uint32 _maxConversionFee
-    ) public StandardPoolConverter(_anchor, _registry, _maxConversionFee) {}
+    ) public StandardPoolConverter(_anchor, _registry, _maxConversionFee) {
+        _rateN = 1;
+        _rateD = 1;
+    }
 
     /**
      * @dev returns the converter type
@@ -30,56 +36,210 @@ contract StaticPoolConverter is StandardPoolConverter {
         return 4;
     }
 
-    function crossReserveTargetAmount(
-        uint256 _sourceReserveBalance,
-        uint256 _targetReserveBalance,
-        uint256 _amount
-    ) internal pure override returns (uint256) {
-        // validate input
-        require(_sourceReserveBalance > 0 && _targetReserveBalance > 0, "ERR_INVALID_RESERVE_BALANCE");
-
-        return _amount;
+    /**
+     * @dev returns the rate
+     *
+     * @return rate numerator
+     * @return rate denominator
+     */
+    function rate() public view returns (uint128, uint128) {
+        return (_rateN, _rateD);
     }
 
-    function fundCost(
-        uint256 _supply,
-        uint256 _reserveBalance,
-        uint256 _amount
-    ) internal pure override returns (uint256) {
-        // validate input
-        require(_supply > 0, "ERR_INVALID_SUPPLY");
-        require(_reserveBalance > 0, "ERR_INVALID_RESERVE_BALANCE");
-
-        return _amount;
+    /**
+     * @dev sets the rate
+     * can be executed only by the owner of the converter
+     *
+     * @param rateN rate numerator
+     * @param rateD rate denominator
+     */
+    function setRate(uint128 rateN, uint128 rateD) public ownerOnly {
+        require(rateN > 0 && rateD > 0, "ERR_INVALID_RATE");
+        _rateN = rateN;
+        _rateD = rateD;
     }
 
-    function fundSupplyAmount(
-        uint256 _supply,
-        uint256 _reserveBalance,
+    /**
+     * @dev returns the expected amount and expected fee for converting one reserve to another
+     *
+     * @param _sourceToken address of the source reserve token contract
+     * @param _targetToken address of the target reserve token contract
+     * @param _amount      amount of source reserve tokens converted
+     *
+     * @return expected amount in units of the target reserve token
+     * @return expected fee in units of the target reserve token
+     */
+    function targetAmountAndFee(
+        IERC20Token _sourceToken,
+        IERC20Token _targetToken,
         uint256 _amount
-    ) internal pure override returns (uint256) {
-        // validate input
-        require(_supply > 0, "ERR_INVALID_SUPPLY");
-        require(_reserveBalance > 0, "ERR_INVALID_RESERVE_BALANCE");
+    ) public view override active returns (uint256, uint256) {
+        uint256 sourceId = __reserveIds[_sourceToken] - 1;
+        uint256 targetId = __reserveIds[_targetToken] - 1;
+    
+        uint128[2] memory factors = [_rateN, _rateD];
 
-        return _amount;
+        uint256 amount = _amount.mul(factors[sourceId]).div(factors[targetId]);
+
+        uint256 fee = calculateFee(amount);
+
+        return (amount - fee, fee);
     }
 
-    function liquidateReserveAmount(
-        uint256 _supply,
-        uint256 _reserveBalance,
+    /**
+     * @dev returns the required amount and expected fee for converting one reserve to another
+     *
+     * @param _sourceToken address of the source reserve token contract
+     * @param _targetToken address of the target reserve token contract
+     * @param _amount      amount of target reserve tokens desired
+     *
+     * @return required amount in units of the source reserve token
+     * @return expected fee in units of the target reserve token
+     */
+    function sourceAmountAndFee(
+        IERC20Token _sourceToken,
+        IERC20Token _targetToken,
         uint256 _amount
-    ) internal pure override returns (uint256) {
-        // validate input
-        require(_supply > 0, "ERR_INVALID_SUPPLY");
-        require(_reserveBalance > 0, "ERR_INVALID_RESERVE_BALANCE");
-        require(_amount <= _supply, "ERR_INVALID_AMOUNT");
+    ) public view override active returns (uint256, uint256) {
+        uint256 sourceId = __reserveIds[_sourceToken] - 1;
+        uint256 targetId = __reserveIds[_targetToken] - 1;
+    
+        uint128[2] memory factors = [_rateN, _rateD];
 
-        // special case for 0 amount
-        if (_amount == 0) {
-            return 0;
+        uint256 fee = calculateFeeInv(_amount);
+
+        uint256 amount = _amount.add(fee).mul(factors[targetId]).div(factors[sourceId]);
+
+        return (amount, fee);
+    }
+
+    /**
+     * @dev increases the pool's liquidity and mints new shares in the pool to the caller
+     *
+     * @param _reserveTokens   address of each reserve token
+     * @param _reserveAmounts  amount of each reserve token
+     * @param _minReturn       token minimum return-amount
+     *
+     * @return amount of pool tokens issued
+     */
+    function addLiquidity(
+        IERC20Token[] memory _reserveTokens,
+        uint256[] memory _reserveAmounts,
+        uint256 _minReturn
+    ) public payable override protected active returns (uint256) {
+        // verify the user input
+        verifyLiquidityInput(_reserveTokens, _reserveAmounts, _minReturn);
+
+        // if one of the reserves is ETH, then verify that the input amount of ETH is equal to the input value of ETH
+        for (uint256 i = 0; i < 2; i++) {
+            if (_reserveTokens[i] == ETH_RESERVE_ADDRESS) {
+                require(_reserveAmounts[i] == msg.value, "ERR_ETH_AMOUNT_MISMATCH");
+            }
         }
 
-        return _amount;
+        // if the input value of ETH is larger than zero, then verify that one of the reserves is ETH
+        if (msg.value > 0) {
+            require(__reserveIds[ETH_RESERVE_ADDRESS] != 0, "ERR_NO_ETH_RESERVE");
+        }
+
+        // save a local copy of the pool token
+        IDSToken poolToken = IDSToken(address(anchor));
+
+        // get the total supply
+        uint256 totalSupply = poolToken.totalSupply();
+
+        // sync the balances to ensure no mismatch
+        syncReserveBalances(msg.value);
+
+        uint256[2] memory oldReserveBalances;
+        uint256[2] memory newReserveBalances;
+        (oldReserveBalances[0], oldReserveBalances[1]) = reserveBalances();
+
+        // calculate the amount of pool tokens to mint
+        uint256 amount;
+        uint256[] memory reserveAmounts = new uint256[](2);
+
+        if (totalSupply == 0) {
+            for (uint256 i = 0; i < 2; i++) {
+                reserveAmounts[i] = _reserveAmounts[i];
+            }
+            amount = MathEx.geometricMean(reserveAmounts);
+        } else {
+            uint256 n = reserveAmounts[0].mul(_rateN).add(reserveAmounts[1]).mul(_rateD);
+            uint256 d = oldReserveBalances[0].mul(_rateN).add(oldReserveBalances[1]).mul(_rateD);
+            amount = totalSupply.mul(n).div(d);
+        }
+
+        uint256 newPoolTokenSupply = totalSupply.add(amount);
+        for (uint256 i = 0; i < 2; i++) {
+            IERC20Token reserveToken = _reserveTokens[i];
+            uint256 reserveAmount = reserveAmounts[i];
+            require(reserveAmount > 0, "ERR_ZERO_TARGET_AMOUNT");
+            assert(reserveAmount <= _reserveAmounts[i]);
+
+            // transfer each one of the reserve amounts from the user to the pool
+            if (reserveToken != ETH_RESERVE_ADDRESS) {
+                // ETH has already been transferred as part of the transaction
+                safeTransferFrom(reserveToken, msg.sender, address(this), reserveAmount);
+            } else if (_reserveAmounts[i] > reserveAmount) {
+                // transfer the extra amount of ETH back to the user
+                msg.sender.transfer(_reserveAmounts[i] - reserveAmount);
+            }
+
+            // save the new reserve balance
+            newReserveBalances[i] = oldReserveBalances[i].add(reserveAmount);
+
+            emit LiquidityAdded(msg.sender, reserveToken, reserveAmount, newReserveBalances[i], newPoolTokenSupply);
+
+            // dispatch the `TokenRateUpdate` event for the pool token
+            emit TokenRateUpdate(poolToken, reserveToken, newReserveBalances[i], newPoolTokenSupply);
+        }
+
+        // set the reserve balances
+        setReserveBalances(1, 2, newReserveBalances[0], newReserveBalances[1]);
+
+        // verify that the equivalent amount of tokens is equal to or larger than the user's expectation
+        require(amount >= _minReturn, "ERR_RETURN_TOO_LOW");
+
+        // issue the tokens to the user
+        poolToken.issue(msg.sender, amount);
+
+        // return the amount of pool tokens issued
+        return amount;
+    }
+
+    /**
+     * @dev verifies that a given array of tokens is identical to the converter's array of reserve tokens
+     * we take this input in order to allow specifying the corresponding reserve amounts in any order
+     * this function rearranges the input arrays according to the converter's array of reserve tokens
+     *
+     * @param _reserveTokens   array of reserve tokens
+     * @param _reserveAmounts  array of reserve amounts
+     * @param _amount          token amount
+     *
+     * @return true if the function has rearranged the input arrays; false otherwise
+     */
+    function verifyLiquidityInput(
+        IERC20Token[] memory _reserveTokens,
+        uint256[] memory _reserveAmounts,
+        uint256 _amount
+    ) internal view override returns (bool) {
+        require(_amount > 0, "ERR_ZERO_AMOUNT");
+
+        uint256 reserve0Id = __reserveIds[_reserveTokens[0]];
+        uint256 reserve1Id = __reserveIds[_reserveTokens[1]];
+
+        if (reserve0Id == 2 && reserve1Id == 1) {
+            IERC20Token tempReserveToken = _reserveTokens[0];
+            _reserveTokens[0] = _reserveTokens[1];
+            _reserveTokens[1] = tempReserveToken;
+            uint256 tempReserveAmount = _reserveAmounts[0];
+            _reserveAmounts[0] = _reserveAmounts[1];
+            _reserveAmounts[1] = tempReserveAmount;
+            return true;
+        }
+
+        require(reserve0Id == 1 && reserve1Id == 2, "ERR_INVALID_RESERVE");
+        return false;
     }
 }
