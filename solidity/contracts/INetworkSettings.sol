1deb7f505f4bf4d9c9405ced9db23514755ba304
Barak Manos <barakman@yahoo.com>
2021-03-07 16:12:44 +0200

    Add a getter function for each state variable.

--- solidity/contracts/INetworkSettings.sol
+++ solidity/contracts/INetworkSettings.sol
@@ -3,4 +3,6 @@ pragma solidity 0.6.12;
 
 interface INetworkSettings {
     function feeParams() external view returns (address, uint32);
+    function networkFeeWallet() external view returns (address);
+    function networkFee() external view returns (uint32);
 }
--- solidity/contracts/NetworkSettings.sol
+++ solidity/contracts/NetworkSettings.sol
@@ -15,9 +15,9 @@ contract NetworkSettings is INetworkSettings, Owned, Utils {
     /**
      * @dev initializes a new NetworkSettings contract
      */
-    constructor(address networkFeeWallet, uint32 networkFee) validAddress(networkFeeWallet) validPortion(networkFee) public {
-        _networkFeeWallet = networkFeeWallet;
-        _networkFee = networkFee;
+    constructor(address networkFeeWalletVal, uint32 networkFeeVal) validAddress(networkFeeWalletVal) validPortion(networkFeeVal) public {
+        _networkFeeWallet = networkFeeWalletVal;
+        _networkFee = networkFeeVal;
     }
 
     /**
@@ -31,22 +31,40 @@ contract NetworkSettings is INetworkSettings, Owned, Utils {
     }
 
     /**
+     * @dev returns the network fee wallet
+     *
+     * @return network fee wallet
+     */
+    function networkFeeWallet() external view override returns (address) {
+        return _networkFeeWallet;
+    }
+
+    /**
+     * @dev returns the network fee
+     *
+     * @return network fee in ppm units
+     */
+    function networkFee() external view override returns (uint32) {
+        return _networkFee;
+    }
+
+    /**
      * @dev sets the network fee wallet
      * can be executed only by the owner
      *
-     * @param networkFeeWallet network fee wallet
+     * @param networkFeeWalletVal network fee wallet
      */
-    function setNetworkFeeWallet(address networkFeeWallet) external ownerOnly validAddress(networkFeeWallet) {
-        _networkFeeWallet = networkFeeWallet;
+    function setNetworkFeeWallet(address networkFeeWalletVal) external ownerOnly validAddress(networkFeeWalletVal) {
+        _networkFeeWallet = networkFeeWalletVal;
     }
 
     /**
      * @dev sets the network fee
      * can be executed only by the owner
      *
-     * @param networkFee network fee in ppm units
+     * @param networkFeeVal network fee in ppm units
      */
-    function setNetworkFee(uint32 networkFee) external ownerOnly validPortion(networkFee) {
-        _networkFee = networkFee;
+    function setNetworkFee(uint32 networkFeeVal) external ownerOnly validPortion(networkFeeVal) {
+        _networkFee = networkFeeVal;
     }
 }
