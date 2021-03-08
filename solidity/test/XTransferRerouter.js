2f32c4cb2572dd1c0d40d81d7f0d12a0a143ae7a
Maxime Aubanel <maximeauba@gmail.com>
2021-03-09 00:02:34 +0100

    Update XTransferRerouter to hardhat

--- solidity/test/XTransferRerouter.js
+++ solidity/test/XTransferRerouter.js
@@ -1,46 +1,58 @@
-const { accounts, defaultSender, contract } = require('@openzeppelin/test-environment');
-const { expect } = require('../../chai-local');
-const { expectRevert, expectEvent, BN } = require('@openzeppelin/test-helpers');
+const { expect } = require('chai');
 
-const XTransferRerouter = contract.fromArtifact('XTransferRerouter');
+const { BigNumber } = require('ethers');
+
+const XTransferRerouter = ethers.getContractFactory('XTransferRerouter');
+
+const EOS_ADDRESS = '0x3c69a194aaf415ba5d6afca734660d0a3d45acdc05d54cd1ca89a8988e7625b4';
+const EOS_BLOCKCHAIN = '0x4e8ebbefa452077428f93c9520d3edd60594ff452a29ac7d2ccc11d47f3ab95b';
+
+let accounts;
+let nonOwner;
+let receiver;
+let txId;
 
 describe('XTransferRerouter', () => {
-    const owner = defaultSender;
-    const nonOwner = accounts[5];
-    const receiver = accounts[1];
-    const txId = new BN(123);
+    before(async () => {
+        accounts = await ethers.getSigners();
 
-    const EOS_ADDRESS = '0x3c69a194aaf415ba5d6afca734660d0a3d45acdc05d54cd1ca89a8988e7625b4';
-    const EOS_BLOCKCHAIN = '0x4e8ebbefa452077428f93c9520d3edd60594ff452a29ac7d2ccc11d47f3ab95b';
+        owner = accounts[0];
+        nonOwner = accounts[5];
+        receiver = accounts[1];
+        txId = BigNumber.from(123);
+    });
 
     it("verify that a user can't call rerouteTx when rerouting is disabled", async () => {
-        const txRouter = await XTransferRerouter.new(false, { from: owner });
+        const txRouter = await (await XTransferRerouter).deploy(false);
 
-        await expectRevert(txRouter.rerouteTx(txId, EOS_ADDRESS, EOS_BLOCKCHAIN, { from: receiver }), 'ERR_DISABLED');
+        await expect(txRouter.connect(receiver).rerouteTx(txId, EOS_ADDRESS, EOS_BLOCKCHAIN)).to.be.revertedWith(
+            'ERR_DISABLED'
+        );
     });
 
     it('verify that calling rerouteTx emits an event properly', async () => {
-        const txRouter = await XTransferRerouter.new(true, { from: owner });
-        const res = await txRouter.rerouteTx(txId, EOS_ADDRESS, EOS_BLOCKCHAIN, { from: receiver });
+        const txRouter = await (await XTransferRerouter).deploy(true);
 
-        expectEvent(res, 'TxReroute', { _txId: txId, _toBlockchain: EOS_ADDRESS, _to: EOS_BLOCKCHAIN });
+        expect(await txRouter.connect(receiver).rerouteTx(txId, EOS_ADDRESS, EOS_BLOCKCHAIN))
+            .to.emit(txRouter, 'TxReroute')
+            .withArgs(txId, EOS_ADDRESS, EOS_BLOCKCHAIN);
     });
 
     it("verify that a non-owner can't call enableRerouting", async () => {
-        const txRouter = await XTransferRerouter.new(false, { from: owner });
+        const txRouter = await (await XTransferRerouter).deploy(false);
 
-        await expectRevert(txRouter.enableRerouting(true, { from: nonOwner }), 'ERR_ACCESS_DENIED');
+        await expect(txRouter.connect(nonOwner).enableRerouting(true)).to.be.revertedWith('ERR_ACCESS_DENIED');
     });
 
     it('verify that the owner can call enableRerouting', async () => {
-        const txRouter = await XTransferRerouter.new(false, { from: owner });
+        const txRouter = await (await XTransferRerouter).deploy(false);
 
-        const prevReroutingEnabled = await txRouter.reroutingEnabled.call();
-        expect(prevReroutingEnabled).to.be.false();
+        const prevReroutingEnabled = await txRouter.reroutingEnabled();
+        expect(prevReroutingEnabled).to.be.false;
 
-        await txRouter.enableRerouting(true, { from: owner });
+        await txRouter.connect(owner).enableRerouting(true);
 
-        const reroutingEnabled = await txRouter.reroutingEnabled.call();
-        expect(reroutingEnabled).to.be.true();
+        const reroutingEnabled = await txRouter.reroutingEnabled();
+        expect(reroutingEnabled).to.be.true;
     });
 });
