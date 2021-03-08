da4327a07ee47a49999f3acda92a802037484810
Maxime Aubanel <maximeauba@gmail.com>
2021-03-08 19:17:42 +0100

    Update ContractRegistry test to hardhat suite

--- solidity/test/helpers/Constants.js
+++ solidity/test/helpers/Constants.js
@@ -1,25 +1,23 @@
-const { web3 } = require('@openzeppelin/test-environment');
-
 module.exports = {
     registry: {
-        CONTRACT_REGISTRY: web3.utils.asciiToHex('ContractRegistry'),
-        BANCOR_NETWORK: web3.utils.asciiToHex('BancorNetwork'),
-        BANCOR_FORMULA: web3.utils.asciiToHex('BancorFormula'),
-        CONVERTER_FACTORY: web3.utils.asciiToHex('ConverterFactory'),
-        CONVERSION_PATH_FINDER: web3.utils.asciiToHex('ConversionPathFinder'),
-        CONVERTER_UPGRADER: web3.utils.asciiToHex('BancorConverterUpgrader'),
-        CONVERTER_REGISTRY: web3.utils.asciiToHex('BancorConverterRegistry'),
-        CONVERTER_REGISTRY_DATA: web3.utils.asciiToHex('BancorConverterRegistryData'),
-        BNT_TOKEN: web3.utils.asciiToHex('BNTToken'),
-        BANCOR_X: web3.utils.asciiToHex('BancorX'),
-        BANCOR_X_UPGRADER: web3.utils.asciiToHex('BancorXUpgrader')
+        CONTRACT_REGISTRY: ethers.utils.formatBytes32String('ContractRegistry'),
+        BANCOR_NETWORK: ethers.utils.formatBytes32String('BancorNetwork'),
+        BANCOR_FORMULA: ethers.utils.formatBytes32String('BancorFormula'),
+        CONVERTER_FACTORY: ethers.utils.formatBytes32String('ConverterFactory'),
+        CONVERSION_PATH_FINDER: ethers.utils.formatBytes32String('ConversionPathFinder'),
+        CONVERTER_UPGRADER: ethers.utils.formatBytes32String('BancorConverterUpgrader'),
+        CONVERTER_REGISTRY: ethers.utils.formatBytes32String('BancorConverterRegistry'),
+        CONVERTER_REGISTRY_DATA: ethers.utils.formatBytes32String('BancorConverterRegistryData'),
+        BNT_TOKEN: ethers.utils.formatBytes32String('BNTToken'),
+        BANCOR_X: ethers.utils.formatBytes32String('BancorX'),
+        BANCOR_X_UPGRADER: ethers.utils.formatBytes32String('BancorXUpgrader')
     },
 
     roles: {
-        ROLE_OWNER: web3.utils.keccak256('ROLE_OWNER'),
-        ROLE_GOVERNOR: web3.utils.keccak256('ROLE_GOVERNOR'),
-        ROLE_MINTER: web3.utils.keccak256('ROLE_MINTER'),
-        ROLE_SEEDER: web3.utils.keccak256('ROLE_SEEDER')
+        ROLE_OWNER: ethers.utils.id('ROLE_OWNER'),
+        ROLE_GOVERNOR: ethers.utils.id('ROLE_GOVERNOR'),
+        ROLE_MINTER: ethers.utils.id('ROLE_MINTER'),
+        ROLE_SEEDER: ethers.utils.id('ROLE_SEEDER')
     },
 
     ETH_RESERVE_ADDRESS: '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE'
