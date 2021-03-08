086a7dc0f24d9a1cd23f1307cdd1a9a0f7612c6a
Maxime Aubanel <maximeauba@gmail.com>
2021-03-08 18:08:41 +0100

    Add hardhat + base folder architecture

---
+++ hardhat.config.js
@@ -0,0 +1,27 @@
+require('@nomiclabs/hardhat-waffle');
+require('@nomiclabs/hardhat-ethers');
+
+require('solidity-coverage');
+
+module.exports = {
+    paths: {
+        sources: './solidity/contracts',
+        tests: './solidity/test',
+        cache: './cache',
+        artifacts: './solidity/build/contracts'
+    },
+    solidity: {
+        version: '0.6.12',
+        settings: {
+            optimizer: {
+                enabled: true,
+                runs: 200
+            }
+        }
+    },
+    mocha: {
+        timeout: 600000,
+        color: true,
+        slow: 30000
+    }
+};
