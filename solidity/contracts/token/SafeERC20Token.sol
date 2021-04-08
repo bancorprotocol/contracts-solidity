b8635c7a90759005ef75bbca201a32bf141a2881
Leonid Beder <leonid.beder@gmail.com>
2021-04-08 15:48:36 +0300

    Add documentation and tests to SafeERC20Token

--- solidity/contracts/token/SafeERC20Token.sol
+++ solidity/contracts/token/SafeERC20Token.sol
@@ -4,34 +4,66 @@ pragma solidity 0.6.12;
 import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 
+/**
+ * @dev Extends the SafeERC20 library with additional operations
+ */
 library SafeERC20Token {
     using SafeERC20 for IERC20;
 
+    /**
+     * @dev transfers ERC20 token using the SafeERC20 library
+     *
+     * @param token the address of the token to transfer
+     * @param to the destination address to transfer the amount to
+     * @param amount the amount to transfer
+     */
     function safeTransfer(
         IERC20 token,
         address to,
-        uint256 value
+        uint256 amount
     ) internal {
-        token.safeTransfer(to, value);
+        token.safeTransfer(to, amount);
     }
 
+    /**
+     * @dev transfers ERC20 token using the SafeERC20 library
+     *
+     * @param token the address of the token to transfer
+     * @param from the source address to transfer the amount from
+     * @param to the destination address to transfer the amount to
+     * @param amount the amount to transfer
+     */
     function safeTransferFrom(
         IERC20 token,
         address from,
         address to,
-        uint256 value
+        uint256 amount
     ) internal {
-        token.safeTransferFrom(from, to, value);
+        token.safeTransferFrom(from, to, amount);
     }
 
+    /**
+     * @dev approves ERC20 token transfer using the SafeERC20 library
+     *
+     * @param token the address of the token to approve
+     * @param spender the address allowed to spend
+     * @param amount the allowed amount to spend
+     */
     function safeApprove(
         IERC20 token,
         address spender,
-        uint256 value
+        uint256 amount
     ) internal {
-        token.safeApprove(spender, value);
+        token.safeApprove(spender, amount);
     }
 
+    /**
+     * @dev ensures that the spender has sufficient allowance
+     *
+     * @param token the address of the token to ensure
+     * @param spender the address allowed to spend
+     * @param amount the allowed amount to spend
+     */
     function ensureAllowance(
         IERC20 token,
         address spender,
