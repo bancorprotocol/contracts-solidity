0cb10ea37cbfab907e53a6e069d8a23bbe1dc994
Leonid Beder <leonid.beder@gmail.com>
2022-05-11 10:44:34 +0100

    Update format and data

--- utils/staking-rewards/snapshot.ts
+++ utils/staking-rewards/snapshot.ts
@@ -10,6 +10,7 @@ const { solidityKeccak256, keccak256 } = utils;
 
 const ETHEREUM_PROVIDER_URL = 'ws://localhost:8545';
 const START_BLOCK = 11039642;
+const END_BLOCK = 14745043;
 const BATCH_SIZE = 1000;
 
 const LIQUIDITY_PROTECTION_STORE_ADDRESS = '0xf5fab5dbd2f3bf675de4cb76517d4767013cfb55';
@@ -24,17 +25,15 @@ const init = () => {
 };
 
 const getProviders = async () => {
-    const toBlock = await provider.getBlockNumber();
-
-    console.log(`Getting all historic providers from ${START_BLOCK} to ${toBlock}...`);
+    console.log(`Getting all historic providers from ${START_BLOCK} to ${END_BLOCK}...`);
     console.log();
 
     const store = LiquidityProtectionStore.connect(LIQUIDITY_PROTECTION_STORE_ADDRESS, provider);
 
     const providers = new Set<string>();
 
-    for (let i = START_BLOCK; i < toBlock; i += BATCH_SIZE) {
-        const endBlock = Math.min(i + BATCH_SIZE - 1, toBlock);
+    for (let i = START_BLOCK; i < END_BLOCK; i += BATCH_SIZE) {
+        const endBlock = Math.min(i + BATCH_SIZE - 1, END_BLOCK);
 
         console.log(`Querying all ProtectionAdded and ProtectionUpdated events from ${i} to ${endBlock}...`);
 
@@ -52,7 +51,12 @@ const getProviders = async () => {
     return providers;
 };
 
-type ProvidersWithRewards = Record<string, BigNumber>;
+interface Rewards {
+    claimable: BigNumber;
+    totalClaimed: BigNumber;
+}
+
+type ProvidersWithRewards = Record<string, Rewards>;
 
 const filterProvidersWithRewards = async (providers: Set<string>) => {
     console.log('Filtering providers with pending rewards...');
@@ -63,24 +67,40 @@ const filterProvidersWithRewards = async (providers: Set<string>) => {
     const stakingRewards = StakingRewards.connect(STAKING_REWARDS_ADDRESS, provider);
 
     for (const provider of providers) {
-        const pendingRewards = await stakingRewards.pendingRewards(provider);
-        if (pendingRewards.gt(0)) {
-            providersWithRewards[provider] = pendingRewards;
+        const claimable = await stakingRewards.pendingRewards(provider);
+        const totalClaimed = await stakingRewards.totalClaimedRewards(provider);
+        if (claimable.gt(0) || totalClaimed.gt(0)) {
+            providersWithRewards[provider] = { claimable, totalClaimed };
         }
     }
 
     return providersWithRewards;
 };
 
-const saveCSV = (providers: ProvidersWithRewards, outputPath: string) => {
-    console.log(`Saving providers CSV as ${outputPath}...`);
+const saveSnapshot = (providers: ProvidersWithRewards, outputPath: string) => {
+    console.log(`Saving providers snapshot as ${outputPath}...`);
     console.log();
 
-    const data = Object.entries(providers)
-        .map(([provider, pendingRewards]) => [provider, pendingRewards.toString()].join(','))
-        .join('\n');
+    let data: Record<string, [string, string]> = {};
+    Object.entries(providers).forEach(
+        ([provider, { claimable, totalClaimed }]) => (data[provider] = [claimable.toString(), totalClaimed.toString()])
+    );
 
-    fs.writeFileSync(outputPath, data, 'utf-8');
+    fs.writeFileSync(
+        outputPath,
+        JSON.stringify(
+            providers,
+            (_, value) => {
+                const { type, hex } = value;
+                if (type === 'BigNumber') {
+                    return BigNumber.from(hex).toString();
+                }
+
+                return value;
+            },
+            4
+        )
+    );
 };
 
 const saveMerkleTree = (providers: ProvidersWithRewards, outputPath: string) => {
@@ -89,15 +109,15 @@ const saveMerkleTree = (providers: ProvidersWithRewards, outputPath: string) => 
 
     const merkleTree = new MerkleTree(
         Object.entries(providers).map(
-            ([provider, pendingRewards]) =>
-                solidityKeccak256(['address', 'uint256'], [provider, pendingRewards.toString()]).slice(2),
+            ([provider, { claimable }]) =>
+                solidityKeccak256(['address', 'uint256'], [provider, claimable.toString()]).slice(2),
             'hex'
         ),
         keccak256,
         { sortPairs: true }
     );
 
-    fs.writeFileSync(outputPath, JSON.stringify({ root: merkleTree.getHexRoot(), tree: merkleTree }, null, 4));
+    fs.writeFileSync(outputPath, JSON.stringify({ root: merkleTree.getHexRoot(), tree: merkleTree }));
 };
 
 const main = async () => {
@@ -108,7 +128,7 @@ const main = async () => {
 
     const timestamp = new Date().toISOString();
 
-    saveCSV(providersWithRewards, `./snapshot-${timestamp}.csv`);
+    saveSnapshot(providersWithRewards, `./snapshot-${timestamp}.json`);
     saveMerkleTree(providersWithRewards, `./snapshot-merkle-tree-${timestamp}.json`);
 };
 
